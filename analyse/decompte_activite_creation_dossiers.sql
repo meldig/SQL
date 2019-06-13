34ab303da631b8dfc4518487b0e5f2c6356bd6a4
Jean-Roc Morreale <jr.morreale@enoreth.net>
2019-06-13 16:32:52 +0200

    groupement par utilisateur

--- analyse/decompte_activite_creation_dossiers.sql
+++ analyse/decompte_activite_creation_dossiers.sql
@@ -1,19 +1,21 @@
 -- dossiers créés dans la journée
 SELECT
 	"NOMUSU",
-	"DOS_DC"
+	COUNT("NOMUSU") AS NBR
 FROM GEO.TA_GG_DOSSIER
 JOIN "ANNU"."POPULATIONS" ON TA_GG_DOSSIER.USER_ID = POPULATIONS.CODPOP
 WHERE "DOS_DC" >= trunc(sysdate)
 ORDER BY "DOS_DC"
+ORDER BY "NBR" DESC
 
 -- dossiers créés dans le mois courant
 SELECT
 	"NOMUSU",
-	"DOS_DC"
+	COUNT("NOMUSU") AS NBR
 FROM GEO.TA_GG_DOSSIER
 JOIN "ANNU"."POPULATIONS" ON TA_GG_DOSSIER.USER_ID = POPULATIONS.CODPOP
 WHERE
   extract(year from DOS_DC) = extract(year from sysdate) AND
   extract(month from DOS_DC) = extract(month from sysdate)
-ORDER BY "DOS_DC"
+GROUP BY "NOMUSU"
+ORDER BY "NBR" DESC
