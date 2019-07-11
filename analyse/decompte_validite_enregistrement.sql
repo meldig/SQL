219697471fc553ce6d0103d997ff31689c782e8c
Jean-Roc Morreale <jr.morreale@enoreth.net>
2019-07-11 10:30:59 +0200

    correction de l'indication du type de géométrie

--- analyse/decompte_validite_enregistrement.sql
+++ analyse/decompte_validite_enregistrement.sql
@@ -1,7 +1,16 @@
--- décompte par état de validité de l'enregistrement
+-- décompte par état de validité de l'enregistrement (0 = valide)
 SELECT
-  GEO_ON_VALIDE,
+  'surfaces' AS type_geom,
+	GEO_ON_VALIDE,
   COUNT(OBJECTID)
 FROM
  GEO.TA_SUR_TOPO_G
-GROUP BY GEO_ON_VALIDE;
+GROUP BY GEO_ON_VALIDE
+UNION
+SELECT
+  'lignes' AS type_geom,
+	GEO_ON_VALIDE,
+  COUNT(OBJECTID)
+FROM
+ GEO.TA_LIG_TOPO_G
+GROUP BY GEO_ON_VALIDE
