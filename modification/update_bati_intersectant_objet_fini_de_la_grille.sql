9b83b7e22553e0072b78acdac97ea23b7f9ddfa5
baptistejacq <bjacq@lillemetropole.fr>
2019-11-20 09:41:25 +0100

    Create update_bati_intersectant_objet_fini_de_la_grille.sql

---
+++ modification/update_bati_intersectant_objet_fini_de_la_grille.sql
@@ -0,0 +1,27 @@
+/*
+	Objectif du trigger : à chaque fois qu'un élément de la grille (Vérification du bati issu du lidar et du plan de gestion) est mis en "terminé", alors le statut de tous les objets bâtis issus soit du lidar, soit du plan de gestion passent en "terminé" également.
+
+*/
+
+CREATE OR REPLACE TRIGGER ta_diff_carto_lidar
+AFTER UPDATE ON TA_GRILLE
+FOR EACH ROW
+
+BEGIN
+	-- Si l'élément de la grille est tagué en "terminé" (fid_lib_etat) et s'il s'agit d'un élément appartenant à la grille qui nous intéresse (fid_thematique) alors...
+    IF :new.fid_lib_etat = '3' AND :new.fid_thematique = '1' THEN
+       -- Tous les objets bâtis se trouvant à l'intérieur de l'élément de la grille voient leur statut passer à '1', c-a-d "traité".
+       UPDATE GEO.ta_diff_carto_lidar a
+       SET a.STATUT = 2
+       WHERE
+            SDO_ANYINTERACT(a.geom, :new.geom) = 'TRUE';
+    END IF;
+    
+        EXCEPTION
+    WHEN OTHERS THEN
+        mail.sendmail(
+            'bjacq@lillemetropole.fr',
+            SQLERRM,'ERREUR TRIGGER ta_diff_carto_lidar',
+            'trigger@lillemetropole.fr'
+        );
+END;
