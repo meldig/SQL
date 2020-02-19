ae1b8d26868f162c4878caa0bd592e5ff3a5ab75
BaptisteJacq <Baptistejacq@gmail.com>
2020-02-19 18:11:06 +0100

    Update creation_v_communes_ign_2019.sql

--- schema/structure commune/creation_v_communes_ign_2019.sql
+++ schema/structure commune/creation_v_communes_ign_2019.sql
@@ -1,16 +1,16 @@
 /*
-Création de la vue des communes actuelles de la BdTopo de l'IGN
+Création de la vue des 90 communes de la BdTopo de l'IGN 
 */
-DROP VIEW v_communes_actuelles_ign;
+DROP VIEW v_90_communes_ign;
 
--- 1. Création de la vue matérialisée
-CREATE OR REPLACE FORCE VIEW g_referentiel.v_communes_actuelles_ign (
+-- 1. Création de la vue
+CREATE OR REPLACE FORCE VIEW g_referentiel.v_90_communes_ign (
     nom,
     code_insee,
     code_postal,
     geom,
     source,
-    CONSTRAINT "V_COMMUNES_ACTUELLES_IGN_PK" PRIMARY KEY ("CODE_INSEE") DISABLE
+    CONSTRAINT "v_90_communes_ign_PK" PRIMARY KEY ("CODE_INSEE") DISABLE
 )
 AS
  WITH
@@ -33,7 +33,8 @@ AS
     WHERE
         b.fid_libelle = 1
         AND g.fid_zone_administrative = 1
-        AND sysdate BETWEEN g.debut_validite AND g.fin_validite
+        AND g.debut_validite = '01/01/2017'
+        AND g.fin_validite = '01/03/2020'
     ),
     
     v_code_postal AS(
@@ -59,9 +60,9 @@ AS
         a.fid_commune = b.fid_commune;
 
 -- 2. Création des commentaires de table et de colonnes
-COMMENT ON TABLE v_communes_actuelles_ign IS 'Vue proposant les communes actuelles de la MEL extraites de la BdTopo de l''IGN.';
-COMMENT ON COLUMN v_communes_actuelles_ign.NOM IS 'Nom de chaque commune de la MEL.';
-COMMENT ON COLUMN v_communes_actuelles_ign.CODE_INSEE IS 'Code INSEE de chaque commune.';
-COMMENT ON COLUMN v_communes_actuelles_ign.CODE_POSTAL IS 'Code Postal de chaque commune.';
-COMMENT ON COLUMN v_communes_actuelles_ign.GEOM IS 'Géométrie de chaque commune - de type polygone.';
-COMMENT ON COLUMN v_communes_actuelles_ign.SOURCE IS 'Source de la donnée avec l''organisme créateur de la source.';
+COMMENT ON TABLE v_90_communes_ign IS 'Vue proposant les communes actuelles de la MEL extraites de la BdTopo de l''IGN.';
+COMMENT ON COLUMN v_90_communes_ign.NOM IS 'Nom de chaque commune de la MEL.';
+COMMENT ON COLUMN v_90_communes_ign.CODE_INSEE IS 'Code INSEE de chaque commune.';
+COMMENT ON COLUMN v_90_communes_ign.CODE_POSTAL IS 'Code Postal de chaque commune.';
+COMMENT ON COLUMN v_90_communes_ign.GEOM IS 'Géométrie de chaque commune - de type polygone.';
+COMMENT ON COLUMN v_90_communes_ign.SOURCE IS 'Source de la donnée avec l''organisme créateur de la source.';
