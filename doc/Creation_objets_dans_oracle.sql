18c861fddbb9eb919ccf00f85febd184d5eba9d8
BaptisteJacq <Baptistejacq@gmail.com>
2020-02-24 16:23:21 +0100

    Update Creation_objets_dans_oracle.sql

--- doc/Creation_objets_dans_oracle.sql
+++ doc/Creation_objets_dans_oracle.sql
@@ -116,12 +116,68 @@ DROP SEQUENCE S_#TABLE#;
 DELETE FROM USER_SDO_GEOM_METADATA WHERE TABLE_NAME = '#TABLE#';
 
 -----------------------------------------------------------------------------------------------
+<<<<<<< Updated upstream
 ------------------------------- Création de  Trigger-------------------------------------------
 -----------------------------------------------------------------------------------------------
 
 --Règles de nommage :
 	-- Si le trigger se déclenche avant l'insertion (BEFORE INSERT), alors utilisez le préfixe 'B_' ;
 	CREATE OR REPLACE TRIGGER NOM_SCHEMA.A_IXX_TABLE_NAME
+=======
+------------------------------- Création de vue -----------------------------------------------
+-----------------------------------------------------------------------------------------------
+
+CREATE OR REPLACE FORCE VIEW #nom_schema#.#nom_vue# (
+    #champ1#,
+    #champ2#,
+    #champ3#,
+    CONSTRAINT #"nom_vue_PK"# PRIMARY KEY("CHAMP1") DISABLE
+)
+AS
+/*
+SELECT
+	champ1,
+	champ2,
+	champ3
+FROM
+	nom_schema.table_name;
+*/
+
+COMMENT ON TABLE #nom_vue# IS '...';
+COMMENT ON COLUMN #nom_vue#.#champ1# IS '...';
+COMMENT ON COLUMN #nom_vue#.#champ2# IS '...';
+COMMENT ON COLUMN #nom_vue#.#champ3# IS '...';
+
+-----------------------------------------------------------------------------------------------
+------------------------------- Création de vue matérialisée ----------------------------------
+-----------------------------------------------------------------------------------------------
+
+CREATE MATERIALIZED VIEW #nom_schema#.#vm_nom_vue_materialisee# 
+USING INDEX 
+TABLESPACE #nom_tablespace# 
+REFRESH ON DEMAND 
+FORCE  
+DISABLE QUERY REWRITE 
+AS
+/*
+SELECT
+	champ1,
+	champ2,
+	champ3
+FROM
+	nom_schema.table_name;
+*/
+
+COMMENT ON MATERIALIZED VIEW #nom_vue# IS '...';
+COMMENT ON COLUMN #nom_vue#.#champ1# IS '...';
+COMMENT ON COLUMN #nom_vue#.#champ2# IS '...';
+COMMENT ON COLUMN #nom_vue#.#champ3# IS '...';
+
+-----------------------------------------------------------------------------------------------
+------------------------------- Création de  Trigger-------------------------------------------
+-----------------------------------------------------------------------------------------------
+
+CREATE OR REPLACE TRIGGER #NOM_SCHEMA#.#B_XXX_TABLE_NAME#
     BEFORE INSERT ON TABLE_NAME
     FOR EACH ROW
 
@@ -132,25 +188,31 @@ DELETE FROM USER_SDO_GEOM_METADATA WHERE TABLE_NAME = '#TABLE#';
 	    ...
 	END;
 
+--Règles de nommage :
+	-- Si le trigger se déclenche avant l'insertion (BEFORE INSERT), alors utilisez le préfixe 'B_' ;
+	CREATE OR REPLACE TRIGGER #NOM_SCHEMA#.#B_..._TABLE_NAME#
+    BEFORE INSERT ON TABLE_NAME
+
 	-- Si le trigger se déclenche après l'insertion (AFTER INSERT), alors utilisez le préfixe 'A_' ;
-	CREATE OR REPLACE TRIGGER NOM_SCHEMA.A_IXX_TABLE_NAME
+	CREATE OR REPLACE TRIGGER #NOM_SCHEMA#.#A_..._TABLE_NAME#
     AFTER INSERT ON TABLE_NAME
-    FOR EACH ROW
-
-	DECLARE
-		...
-
-	BEGIN
-	    ...
-	END;
 	
-	- S'il s'agit d'une insertion seule, rajoutez 'IXX' -> 'B_IXX_TABLE_NAME' ;
-	- S'il s'agit d'une mise à jour seule, rajoutez 'UXX' -> 'B_UXX_' ;
-	- S'il s'agit d'une Suppression seule, rajoutez 'DXX' -> 'B_DXX_' ;
-	==> Les deux 'XX' placés derrière l'initiale d'insertion/mise à jour/suppression signifie que le trigger marche UNIQUEMENT pour une action ;
-	- Si un trigger peut être déclenché par deux actions, alors, vous pouvez mélanger les préfixes -> 'B_IUX_TABLE_NAME' signifie que le trigger se déclenche avant l'insertion ou la mise de la table dont le nom figure dans le nom du trigger ;
-*/
+	-- S'il s'agit d'une insertion seule, rajoutez 'IXX' -> 'B_IXX_TABLE_NAME' ;
+	CREATE OR REPLACE TRIGGER #NOM_SCHEMA#.#B_IXX_TABLE_NAME#
+    BEFORE INSERT ON TABLE_NAME
 
--- Trigger BEFORE INSERT 
+	-- S'il s'agit d'une mise à jour seule, rajoutez 'UXX' -> 'B_UXX_' ;
+	CREATE OR REPLACE TRIGGER #NOM_SCHEMA#.#B_UXX_TABLE_NAME#
+    BEFORE INSERT ON TABLE_NAME
+
+	-- S'il s'agit d'une Suppression seule, rajoutez 'DXX' -> 'B_DXX_' ;
+	CREATE OR REPLACE TRIGGER #NOM_SCHEMA#.#B_DXX_TABLE_NAME#
+    BEFORE INSERT ON TABLE_NAME
+
+	--> Les deux 'XX' placés derrière l'initiale d'insertion/mise à jour/suppression signifie que le trigger marche UNIQUEMENT pour une action ;
+	
+	-- Si un trigger peut être déclenché par deux actions, alors, vous pouvez mélanger les préfixes -> 'B_IUX_TABLE_NAME' signifie que le trigger se déclenche avant l'insertion ou la mise de la table dont le nom figure dans le nom du trigger ;
+	CREATE OR REPLACE TRIGGER #NOM_SCHEMA#.#B_IUX_TABLE_NAME#
+    BEFORE INSERT ON TABLE_NAME
 
 
