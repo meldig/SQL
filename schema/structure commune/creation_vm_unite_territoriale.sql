fd66c0fdd6cffa20299fa0ad8883ce61ba92f18d
BaptisteJacq <Baptistejacq@gmail.com>
2020-02-25 17:02:39 +0100

    Update creation_vm_unite_territoriale.sql

--- schema/structure commune/creation_vm_unite_territoriale.sql
+++ schema/structure commune/creation_vm_unite_territoriale.sql
@@ -2,21 +2,25 @@
 Création de la table matérialisée des Unités Territoriales (faite à partir de l'aggrégation des communes)
 */
 
-DROP MATERIALIZED VIEW g_referentiel.vm_unite_territoriale;
+DROP MATERIALIZED VIEW g_referentiel.unite_territoriale_mel;
 
 -- 1. Création de la vue matérialisée
-CREATE MATERIALIZED VIEW g_referentiel.vm_unite_territoriale(
-	nom_minuscule,
-    geom
+CREATE MATERIALIZED VIEW g_referentiel.unite_territoriale_mel(
+	objectid,
+  nom_minuscule,
+  nom_majuscule,
+  geom,
+  aire_km2
 )
 REFRESH ON DEMAND
 FORCE
 DISABLE QUERY REWRITE AS
+WITH
+    v_fusion_ut AS(
 SELECT 
     LOWER(d.nom)AS nom_minuscule,
     UPPER(d.nom) AS nom_majuscule,
-    SDO_AGGR_UNION(SDOAGGRTYPE(a.geom, 0.005)) AS geom,
-    ROUND(SDO_GEOM.SDO_AREA(geom, 0.005, 'unit=SQ_KILOMETER'), 2) AS aire_km2,
+    SDO_AGGR_UNION(SDOAGGRTYPE(a.geom, 0.005)) AS geom
 FROM
     g_geo.ta_commune a
     INNER JOIN g_geo.ta_za_communes b ON a.objectid = b.fid_commune
@@ -24,7 +28,18 @@ FROM
     INNER JOIN g_geo.ta_nom d ON c.fid_nom = d.objectid
 WHERE
     d.nom IN('TOURCOING-ARMENTIERES', 'ROUBAIX-VILLENEUVE D''ASCQ', 'LILLE-SECLIN', 'LA BASSEE-MARCQ EN BAROEUL')
-GROUP BY d.nom;
+    AND sysdate BETWEEN b.debut_validite AND b.fin_validite
+GROUP BY d.nom
+    )
+    
+SELECT
+    rownum AS objectid,
+    a.nom_minuscule,
+    a.nom_majuscule,
+    a.geom,
+    ROUND(SDO_GEOM.SDO_AREA(a.geom, 0.005, 'unit=SQ_KILOMETER'), 2) AS aire_km2
+FROM
+    v_fusion_ut a;
 
 -- 2. Création des métadonnées spatiales
 INSERT INTO USER_SDO_GEOM_METADATA(
@@ -34,7 +49,7 @@ INSERT INTO USER_SDO_GEOM_METADATA(
     SRID
 )
 VALUES(
-    'vm_unite_territoriale',
+    'unite_territoriale_mel',
     'geom',
     SDO_DIM_ARRAY(SDO_DIM_ELEMENT('X', 594000, 964000, 0.005),SDO_DIM_ELEMENT('Y', 6987000, 7165000, 0.005)), 
     2154
@@ -42,15 +57,15 @@ VALUES(
 
 -- 3. Création de la clé primaire
 ALTER MATERIALIZED VIEW 
-  vm_unite_territoriale 
+  unite_territoriale_mel 
 ADD CONSTRAINT 
-  vm_unite_territoriale_PK 
+  unite_territoriale_mel_PK 
 PRIMARY KEY (NOM)
 DISABLE;
 
 -- 4. Création de l'index spatial
-CREATE INDEX vm_unite_territoriale_SIDX
-ON vm_unite_territoriale(GEOM)
+CREATE INDEX unite_territoriale_mel_SIDX
+ON unite_territoriale_mel(GEOM)
 INDEXTYPE IS MDSYS.SPATIAL_INDEX
 PARAMETERS(
   'sdo_indx_dims=2, 
@@ -58,3 +73,12 @@ PARAMETERS(
   tablespace=G_ADT_INDX, 
   work_tablespace=DATA_TEMP'
 );
+
+-- 5. Création des commentaires de table et de colonnes
+COMMENT ON MATERIALIZED VIEW g_referentiel.unite_territoriale_mel IS 'Vue matérialisée proposant les Unités Territoriales de la MEL.';
+COMMENT ON COLUMN g_referentiel.unite_territoriale_mel.OBJECTID IS 'Clé primaire de chaque enregistrement.';
+COMMENT ON COLUMN g_referentiel.unite_territoriale_mel.nom_minuscule IS 'Nom des Unités Territoriales en minuscule.';
+COMMENT ON COLUMN g_referentiel.unite_territoriale_mel.nom_majuscule IS 'Nom des Unités Territoriales en majuscule.';
+COMMENT ON COLUMN g_referentiel.unite_territoriale_mel.GEOM IS 'Géométrie de chaque Unités Territoriales.';
+COMMENT ON COLUMN g_referentiel.unite_territoriale_mel.aire_km2 IS 'Surface de chaque commune, municipalité en km² arrondie à deux decimales.';
+COMMENT ON COLUMN g_referentiel.unite_territoriale_mel.SOURCE IS 'Source de la donnée avec l''organisme créateur, la source et son millésime.';
