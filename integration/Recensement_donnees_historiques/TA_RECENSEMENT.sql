b61571b7c1d84437eab50d4d8e0abcfc98504d92
romain-jault <57357284+romain-jault@users.noreply.github.com>
2020-04-09 15:40:08 +0200

    Update TA_RECENSEMENT.sql
    Correction coquilles

--- integration/Recensement_donnees_historiques/TA_RECENSEMENT.sql
+++ integration/Recensement_donnees_historiques/TA_RECENSEMENT.sql
@@ -15,7 +15,7 @@ CREATE TABLE ta_recensement(
 COMMENT ON TABLE g_geo.ta_recensement IS 'Table qui regroupe toutes les valeurs des différents récensements comptabilisées par les communes composant la MEL entre 1876 et 2017.';
 COMMENT ON COLUMN g_geo.ta_recensement.objectid IS 'Clé primaire de la table.';
 COMMENT ON COLUMN g_geo.ta_recensement.fid_code IS 'Clé étrangère vers la table TA_CODE pour connaitre la commune concernée par la valeur du recensement.';
-COMMENT ON COLUMN g_geo.ta_recensement.fid_recensement IS 'Clé étrangère vers la table TA_LIBELLE_COURT pour connaitre la date du recensement.';
+COMMENT ON COLUMN g_geo.ta_recensement.fid_recensement IS 'Clé étrangère vers la table TA_LIBELLE_COURT pour connaitre le recensement concerné.';
 COMMENT ON COLUMN g_geo.ta_recensement.population IS 'Nombre d''habitant recensé.';
 COMMENT ON COLUMN g_geo.ta_recensement.fid_metadonnee IS 'Clé étrangère vers la table TA_METADONNEE pour connaitre les informations sur les données contenues dans la table.';
 
@@ -34,7 +34,7 @@ ALTER TABLE ta_recensement
 	FOREIGN KEY (fid_code)
 	REFERENCES ta_code(objectid);
 
--- 4.2 Clé étrangère du champ fid_nomenclature_recensement vers la table ta_nomenclature
+-- 4.2 Clé étrangère du champ fid_recensement vers la table ta_libelle_court
 
 ALTER TABLE ta_recensement
 	ADD CONSTRAINT ta_recensement_fid_recensement_FK
