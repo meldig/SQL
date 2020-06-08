eba1ef26b8579ba115850a56b7f03b7397bf003e
romain-jault <57357284+romain-jault@users.noreply.github.com>
2020-06-08 17:06:04 +0200

    Update normalisation_recensement_donnees_historique.sql
    Mise à jour de la requête de normalisation des données historique de la population.

--- integration/Recensement_donnees_historiques/normalisation_recensement_donnees_historique.sql
+++ integration/Recensement_donnees_historiques/normalisation_recensement_donnees_historique.sql
@@ -1,27 +1,35 @@
+/*
+Requêtes SQL utilisées pour normaliser les données issues de la base historique des populatons communales. Recensements de la population 1876-2017.
+Cette base fournit les données de populations de 1876 à 2017 pour les communes de France continentale, de 1936 à 2017 pour les communes de Corse et de 1954 ou 1962 à 2017 pour les communes des DOM (hors Mayotte).
+*/
+
+
 -- Insertion des données dans la table TA_RECENSEMENT
 INSERT INTO ta_recensement(fid_code,population,fid_recensement,fid_metadonnee)
--- CTE pour selection les différentes metadonnees suivant le millesime du recensement
-with annee as
-    (
+-- CTE pour selectionner les différentes metadonnees suivant le millesime du recensement
+WITH annee AS (
     SELECT
         m.objectid,
         a.millesime
     FROM
         ta_metadonnee m
-    INNER JOIN ta_source s ON m.fid_source = s.objectid
-    INNER JOIN ta_date_acquisition a ON m.fid_acquisition = a.objectid
+    INNER JOIN
+        ta_source s
+    ON m.fid_source = s.objectid
+    INNER JOIN
+        ta_date_acquisition a
+    ON m.fid_acquisition = a.objectid
     WHERE 
         s.nom_source = 'Historique des populations communales - Recensements de la population 1876-2017'
     ),
 -- CTE pour mettre les données de la table de recensement en forme avant insertion. Mettre les données dans une colonne plutot que d'avoir une colonne par recensement
-    population as
-    (
+    population AS (
     SELECT
         codgeo,
         recensement,
         habitant
     FROM
-    feuil1
+    recensement
     UNPIVOT
         (habitant for (recensement) IN
         (PMUN17 AS 'PMUN17',
@@ -60,9 +68,9 @@ with annee as
 SELECT
     c.objectid AS code,
     p.habitant,
-    lc.objectid AS RECENSEMENT,
+    l.objectid AS RECENSEMENT,
     CASE
-        WHEN lc.libelle_court = 'PMUN17'
+        WHEN lc.valeur = 'PMUN17'
         THEN
             (
             SELECT
@@ -71,7 +79,7 @@ SELECT
                 annee
             WHERE
                 annee.millesime = '01/01/2017')
-        WHEN lc.libelle_court = 'PMUN16'
+        WHEN lc.valeur = 'PMUN16'
         THEN
             (
             SELECT
@@ -80,7 +88,7 @@ SELECT
                 annee
             WHERE
                 annee.millesime = '01/01/2016')
-        WHEN lc.libelle_court = 'PMUN15'
+        WHEN lc.valeur = 'PMUN15'
         THEN
             (
             SELECT
@@ -89,7 +97,7 @@ SELECT
                 annee
             WHERE
                 annee.millesime = '01/01/2015')
-        WHEN lc.libelle_court = 'PMUN14'
+        WHEN lc.valeur = 'PMUN14'
         THEN
             (
             SELECT
@@ -98,7 +106,7 @@ SELECT
                 annee
             WHERE
                 annee.millesime = '01/01/2014')
-        WHEN lc.libelle_court = 'PMUN13'
+        WHEN lc.valeur = 'PMUN13'
         THEN
             (
             SELECT
@@ -107,7 +115,7 @@ SELECT
                 annee
             WHERE
                 annee.millesime = '01/01/2013')
-        WHEN lc.libelle_court = 'PMUN12'
+        WHEN lc.valeur = 'PMUN12'
         THEN
             (
             SELECT
@@ -116,7 +124,7 @@ SELECT
                 annee
             WHERE
                 annee.millesime = '01/01/2012')
-        WHEN lc.libelle_court = 'PMUN11'
+        WHEN lc.valeur = 'PMUN11'
         THEN
             (
             SELECT
@@ -125,7 +133,7 @@ SELECT
                 annee
             WHERE
                 annee.millesime = '01/01/2011')
-        WHEN lc.libelle_court = 'PMUN10'
+        WHEN lc.valeur = 'PMUN10'
         THEN
             (
             SELECT
@@ -134,7 +142,7 @@ SELECT
                 annee
             WHERE
                 annee.millesime = '01/01/2010')
-        WHEN lc.libelle_court = 'PMUN09'
+        WHEN lc.valeur = 'PMUN09'
         THEN
             (
             SELECT
@@ -143,7 +151,7 @@ SELECT
                 annee
             WHERE
                 annee.millesime = '01/01/2009')
-        WHEN lc.libelle_court = 'PMUN08'
+        WHEN lc.valeur = 'PMUN08'
         THEN
             (
             SELECT
@@ -152,7 +160,7 @@ SELECT
                 annee
             WHERE
                 annee.millesime = '01/01/2008')
-        WHEN lc.libelle_court = 'PMUN07'
+        WHEN lc.valeur = 'PMUN07'
         THEN
             (
             SELECT
@@ -161,7 +169,7 @@ SELECT
                 annee
             WHERE
                 annee.millesime = '01/01/2007') 
-        WHEN lc.libelle_court = 'PMUN06'
+        WHEN lc.valeur = 'PMUN06'
         THEN
             (
             SELECT
@@ -170,7 +178,7 @@ SELECT
                 annee
             WHERE
                 annee.millesime = '01/01/2006') 
-        WHEN lc.libelle_court = 'PSDC99'
+        WHEN lc.valeur = 'PSDC99'
         THEN
             (
             SELECT
@@ -179,7 +187,7 @@ SELECT
                 annee
             WHERE
                 annee.millesime = '01/01/1999') 
-        WHEN lc.libelle_court = 'PSDC90'
+        WHEN lc.valeur = 'PSDC90'
         THEN
             (
             SELECT
@@ -188,7 +196,7 @@ SELECT
                 annee
             WHERE
                 annee.millesime = '01/01/1990') 
-        WHEN lc.libelle_court = 'PSDC82'
+        WHEN lc.valeur = 'PSDC82'
         THEN
             (
             SELECT
@@ -197,7 +205,7 @@ SELECT
                 annee
             WHERE
                 annee.millesime = '01/01/1982') 
-        WHEN lc.libelle_court = 'PSDC75'
+        WHEN lc.valeur = 'PSDC75'
         THEN
             (
             SELECT
@@ -206,7 +214,7 @@ SELECT
                 annee
             WHERE
                 annee.millesime = '01/01/1975') 
-        WHEN lc.libelle_court = 'PSDC68'
+        WHEN lc.valeur = 'PSDC68'
         THEN
             (
             SELECT
@@ -215,7 +223,7 @@ SELECT
                 annee
             WHERE
                 annee.millesime = '01/01/1968') 
-        WHEN lc.libelle_court = 'PSDC62'
+        WHEN lc.valeur = 'PSDC62'
         THEN
             (
             SELECT
@@ -224,7 +232,7 @@ SELECT
                 annee
             WHERE
                 annee.millesime = '01/01/1962')
-        WHEN lc.libelle_court = 'PTOT54'
+        WHEN lc.valeur = 'PTOT54'
         THEN
             (
             SELECT
@@ -233,7 +241,7 @@ SELECT
                 annee
             WHERE
                 annee.millesime = '01/01/1954')
-        WHEN lc.libelle_court = 'PTOT36'
+        WHEN lc.valeur = 'PTOT36'
         THEN
             (
             SELECT
@@ -242,7 +250,7 @@ SELECT
                 annee
             WHERE
                 annee.millesime = '01/01/1936')
-        WHEN lc.libelle_court = 'PTOT1931'
+        WHEN lc.valeur = 'PTOT1931'
         THEN
             (
             SELECT
@@ -251,7 +259,7 @@ SELECT
                 annee
             WHERE
                 annee.millesime = '01/01/1931')
-        WHEN lc.libelle_court = 'PTOT1926'
+        WHEN lc.valeur = 'PTOT1926'
         THEN
             (
             SELECT
@@ -260,7 +268,7 @@ SELECT
                 annee
             WHERE
                 annee.millesime = '01/01/1926')
-        WHEN lc.libelle_court = 'PTOT1921'
+        WHEN lc.valeur = 'PTOT1921'
         THEN
             (
             SELECT
@@ -269,7 +277,7 @@ SELECT
                 annee
             WHERE
                 annee.millesime = '01/01/1921')
-        WHEN lc.libelle_court = 'PTOT1911'
+        WHEN lc.valeur = 'PTOT1911'
         THEN
             (
             SELECT
@@ -278,7 +286,7 @@ SELECT
                 annee
             WHERE
                 annee.millesime = '01/01/1911')
-        WHEN lc.libelle_court = 'PTOT1906'
+        WHEN lc.valeur = 'PTOT1906'
         THEN
             (
             SELECT
@@ -287,7 +295,7 @@ SELECT
                 annee
             WHERE
                 annee.millesime = '01/01/1906')
-        WHEN lc.libelle_court = 'PTOT1901'
+        WHEN lc.valeur = 'PTOT1901'
         THEN
             (
             SELECT
@@ -296,7 +304,7 @@ SELECT
                 annee
             WHERE
                 annee.millesime = '01/01/1901')
-        WHEN lc.libelle_court = 'PTOT1896'
+        WHEN lc.valeur = 'PTOT1896'
         THEN
             (
             SELECT
@@ -305,7 +313,7 @@ SELECT
                 annee
             WHERE
                 annee.millesime = '01/01/1896')
-        WHEN lc.libelle_court = 'PTOT1891'
+        WHEN lc.valeur = 'PTOT1891'
         THEN
             (
             SELECT
@@ -314,7 +322,7 @@ SELECT
                 annee
             WHERE
                 annee.millesime = '01/01/1891')
-        WHEN lc.libelle_court = 'PTOT1886'
+        WHEN lc.valeur = 'PTOT1886'
         THEN
             (
             SELECT
@@ -323,7 +331,7 @@ SELECT
                 annee
             WHERE
                 annee.millesime = '01/01/1886')
-        WHEN lc.libelle_court = 'PTOT1881'
+        WHEN lc.valeur = 'PTOT1881'
         THEN
             (
             SELECT
@@ -332,7 +340,7 @@ SELECT
                 annee
             WHERE
                 annee.millesime = '01/01/1881')
-        WHEN lc.libelle_court = 'PTOT1876'
+        WHEN lc.valeur = 'PTOT1876'
         THEN
             (
             SELECT
@@ -344,7 +352,21 @@ SELECT
             END AS metadonnee
 FROM
     population p
-INNER JOIN ta_code c ON p.codgeo = c.code
-INNER JOIN ta_libelle_court lc ON p.recensement = lc.libelle_court
-INNER JOIN ta_libelle l ON c.fid_libelle = l.objectid
+    INNER JOIN ta_code c ON p.codgeo = c.code
+    INNER JOIN ta_libelle cl ON c.fid_libelle = cl.objectid
+    INNER JOIN ta_libelle_long cll ON cl.fid_libelle_long = cll.objectid
+    INNER JOIN ta_famille_libelle cfl ON cll.objectid = cfl.fid_libelle_long
+    INNER JOIN ta_famille cf ON cfl.fid_famille = cf.objectid    
+    INNER JOIN ta_libelle_court lc ON p.recensement = lc.valeur
+    INNER JOIN ta_correspondance_libelle tc ON lc.objectid = tc.fid_libelle_court 
+    INNER JOIN ta_libelle l ON tc.fid_libelle = l.objectid
+    INNER JOIN ta_libelle_long ll ON l.fid_libelle_long = ll.objectid
+    INNER JOIN ta_famille_libelle fl ON ll.objectid = fl.fid_libelle_long
+    INNER JOIN ta_famille f ON fl.fid_famille = f.objectid
+    WHERE
+    f.valeur = 'Recensement'
+    AND
+    cll.valeur = 'code insee'
+    AND
+    cf.valeur = 'Identifiant de zone administrative'
 ;
