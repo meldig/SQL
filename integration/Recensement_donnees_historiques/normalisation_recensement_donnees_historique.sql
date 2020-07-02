4e0b3a08f7805ed0ddb76ec86f2247a8d8a708e3
romain-jault <57357284+romain-jault@users.noreply.github.com>
2020-07-02 16:28:11 +0200

    Update normalisation_recensement_donnees_historique.sql
    Mise à jour du fichier de normalisation des données du recensement.

--- integration/Recensement_donnees_historiques/normalisation_recensement_donnees_historique.sql
+++ integration/Recensement_donnees_historiques/normalisation_recensement_donnees_historique.sql
@@ -9,18 +9,25 @@ INSERT INTO ta_recensement(fid_code,population,fid_lib_recensement,fid_metadonne
 -- CTE pour selectionner les différentes metadonnees suivant le millesime du recensement
 WITH annee AS (
     SELECT
-        m.objectid,
-        a.millesime
+        a.objectid,
+        b.millesime
     FROM
-        ta_metadonnee m
-    INNER JOIN
-        ta_source s
-    ON m.fid_source = s.objectid
-    INNER JOIN
-        ta_date_acquisition a
-    ON m.fid_acquisition = a.objectid
-    WHERE 
-        s.nom_source = 'Historique des populations communales - Recensements de la population 1876-2017'
+        ta_metadonnee a
+    INNER JOIN ta_date_acquisition b ON a.fid_acquisition = b.objectid
+    INNER JOIN ta_source c ON a.fid_source = c.objectid
+    INNER JOIN ta_provenance d ON a.fid_provenance = d.objectid
+    INNER JOIN ta_metadonnee_relation_organisme e ON a.objectid = e.fid_metadonnee
+    INNER JOIN ta_organisme f ON e.fid_organisme = f.objectid
+    WHERE
+        b.date_acquisition = '06/04/2020'
+    AND
+        b.millesime BETWEEN '01/01/1876' AND '01/01/2017'
+    AND
+        c.nom_source = 'Recensements de la population 1876-2017'
+    AND
+        d.url = 'https://www.insee.fr/fr/statistiques/3698339#consulter'
+    AND
+        f.nom_organisme IN ('Institut National de la statistique et des études économiques')
     ),
 -- CTE pour mettre les données de la table de recensement en forme avant insertion. Mettre les données dans une colonne plutot que d'avoir une colonne par recensement
     population AS (
@@ -66,307 +73,181 @@ WITH annee AS (
         )
 -- selection des données avec le fid metadonnee suivant l'année du recensement.
 SELECT
-    c.objectid AS code,
-    p.habitant,
-    l.objectid AS RECENSEMENT,
-    CASE
-        WHEN lc.valeur = 'PMUN17'
-        THEN
-            (
-            SELECT
-                objectid
-            FROM
-                annee
-            WHERE
-                annee.millesime = '01/01/2017')
-        WHEN lc.valeur = 'PMUN16'
-        THEN
-            (
-            SELECT
-                objectid
-            FROM
-                annee
-            WHERE
-                annee.millesime = '01/01/2016')
-        WHEN lc.valeur = 'PMUN15'
-        THEN
-            (
-            SELECT
-                objectid
-            FROM
-                annee
-            WHERE
-                annee.millesime = '01/01/2015')
-        WHEN lc.valeur = 'PMUN14'
-        THEN
-            (
-            SELECT
-                objectid
-            FROM
-                annee
-            WHERE
-                annee.millesime = '01/01/2014')
-        WHEN lc.valeur = 'PMUN13'
-        THEN
-            (
-            SELECT
-                objectid
-            FROM
-                annee
-            WHERE
-                annee.millesime = '01/01/2013')
-        WHEN lc.valeur = 'PMUN12'
-        THEN
-            (
-            SELECT
-                objectid
-            FROM
-                annee
-            WHERE
-                annee.millesime = '01/01/2012')
-        WHEN lc.valeur = 'PMUN11'
-        THEN
-            (
-            SELECT
-                objectid
-            FROM
-                annee
-            WHERE
-                annee.millesime = '01/01/2011')
-        WHEN lc.valeur = 'PMUN10'
-        THEN
-            (
-            SELECT
-                objectid
-            FROM
-                annee
-            WHERE
-                annee.millesime = '01/01/2010')
-        WHEN lc.valeur = 'PMUN09'
-        THEN
-            (
-            SELECT
-                objectid
-            FROM
-                annee
-            WHERE
-                annee.millesime = '01/01/2009')
-        WHEN lc.valeur = 'PMUN08'
-        THEN
-            (
-            SELECT
-                objectid
-            FROM
-                annee
-            WHERE
-                annee.millesime = '01/01/2008')
-        WHEN lc.valeur = 'PMUN07'
-        THEN
-            (
-            SELECT
-                objectid
-            FROM
-                annee
-            WHERE
-                annee.millesime = '01/01/2007') 
-        WHEN lc.valeur = 'PMUN06'
-        THEN
-            (
-            SELECT
-                objectid
-            FROM
-                annee
-            WHERE
-                annee.millesime = '01/01/2006') 
-        WHEN lc.valeur = 'PSDC99'
-        THEN
-            (
-            SELECT
-                objectid
-            FROM
-                annee
-            WHERE
-                annee.millesime = '01/01/1999') 
-        WHEN lc.valeur = 'PSDC90'
-        THEN
-            (
-            SELECT
-                objectid
-            FROM
-                annee
-            WHERE
-                annee.millesime = '01/01/1990') 
-        WHEN lc.valeur = 'PSDC82'
-        THEN
-            (
-            SELECT
-                objectid
-            FROM
-                annee
-            WHERE
-                annee.millesime = '01/01/1982') 
-        WHEN lc.valeur = 'PSDC75'
-        THEN
-            (
-            SELECT
-                objectid
-            FROM
-                annee
-            WHERE
-                annee.millesime = '01/01/1975') 
-        WHEN lc.valeur = 'PSDC68'
-        THEN
-            (
-            SELECT
-                objectid
-            FROM
-                annee
-            WHERE
-                annee.millesime = '01/01/1968') 
-        WHEN lc.valeur = 'PSDC62'
-        THEN
-            (
-            SELECT
-                objectid
-            FROM
-                annee
-            WHERE
-                annee.millesime = '01/01/1962')
-        WHEN lc.valeur = 'PTOT54'
-        THEN
-            (
-            SELECT
-                objectid
-            FROM
-                annee
-            WHERE
-                annee.millesime = '01/01/1954')
-        WHEN lc.valeur = 'PTOT36'
-        THEN
-            (
-            SELECT
-                objectid
-            FROM
-                annee
-            WHERE
-                annee.millesime = '01/01/1936')
-        WHEN lc.valeur = 'PTOT1931'
-        THEN
-            (
-            SELECT
-                objectid
-            FROM
-                annee
-            WHERE
-                annee.millesime = '01/01/1931')
-        WHEN lc.valeur = 'PTOT1926'
-        THEN
-            (
-            SELECT
-                objectid
-            FROM
-                annee
-            WHERE
-                annee.millesime = '01/01/1926')
-        WHEN lc.valeur = 'PTOT1921'
-        THEN
-            (
-            SELECT
-                objectid
-            FROM
-                annee
-            WHERE
-                annee.millesime = '01/01/1921')
-        WHEN lc.valeur = 'PTOT1911'
-        THEN
-            (
-            SELECT
-                objectid
-            FROM
-                annee
-            WHERE
-                annee.millesime = '01/01/1911')
-        WHEN lc.valeur = 'PTOT1906'
-        THEN
-            (
-            SELECT
-                objectid
-            FROM
-                annee
-            WHERE
-                annee.millesime = '01/01/1906')
-        WHEN lc.valeur = 'PTOT1901'
-        THEN
-            (
-            SELECT
-                objectid
-            FROM
-                annee
-            WHERE
-                annee.millesime = '01/01/1901')
-        WHEN lc.valeur = 'PTOT1896'
-        THEN
-            (
-            SELECT
-                objectid
-            FROM
-                annee
-            WHERE
-                annee.millesime = '01/01/1896')
-        WHEN lc.valeur = 'PTOT1891'
-        THEN
-            (
-            SELECT
-                objectid
-            FROM
-                annee
-            WHERE
-                annee.millesime = '01/01/1891')
-        WHEN lc.valeur = 'PTOT1886'
-        THEN
-            (
-            SELECT
-                objectid
-            FROM
-                annee
-            WHERE
-                annee.millesime = '01/01/1886')
-        WHEN lc.valeur = 'PTOT1881'
-        THEN
-            (
-            SELECT
-                objectid
-            FROM
-                annee
-            WHERE
-                annee.millesime = '01/01/1881')
-        WHEN lc.valeur = 'PTOT1876'
-        THEN
-            (
-            SELECT
-                objectid
-            FROM
-                annee
-            WHERE
-                annee.millesime = '01/01/1876')
-            END AS metadonnee
+  a.objectid AS code_insee,
+  b.habitant,
+  g.objectid AS RECENSEMENT,
+  CASE
+    WHEN e.valeur = 'PMUN17'
+      THEN (
+        SELECT objectid
+        FROM annee
+        WHERE annee.millesime = '01/01/2017')
+    WHEN e.valeur = 'PMUN16'
+      THEN (
+        SELECT objectid
+        FROM annee
+        WHERE annee.millesime = '01/01/2016')
+        WHEN e.valeur = 'PMUN15'
+      THEN (
+        SELECT objectid
+        FROM annee
+        WHERE annee.millesime = '01/01/2015')
+    WHEN e.valeur = 'PMUN14'
+      THEN (
+        SELECT objectid
+        FROM annee
+        WHERE annee.millesime = '01/01/2014')
+    WHEN e.valeur = 'PMUN13'
+      THEN (
+        SELECT objectid
+        FROM annee
+        WHERE annee.millesime = '01/01/2013')
+    WHEN e.valeur = 'PMUN12'
+      THEN (
+        SELECT objectid
+        FROM annee
+        WHERE annee.millesime = '01/01/2012')
+    WHEN e.valeur = 'PMUN11'
+      THEN (
+        SELECT objectid
+        FROM annee
+        WHERE annee.millesime = '01/01/2011')
+    WHEN e.valeur = 'PMUN10'
+      THEN (
+        SELECT objectid
+        FROM annee
+        WHERE annee.millesime = '01/01/2010')
+    WHEN e.valeur = 'PMUN09'
+      THEN (
+        SELECT objectid
+        FROM annee
+        WHERE annee.millesime = '01/01/2009')
+    WHEN e.valeur = 'PMUN08'
+      THEN (
+        SELECT objectid
+        FROM annee
+        WHERE annee.millesime = '01/01/2008')
+    WHEN e.valeur = 'PMUN07'
+      THEN (
+        SELECT objectid
+        FROM annee
+        WHERE annee.millesime = '01/01/2007')
+    WHEN e.valeur = 'PMUN06'
+      THEN (
+        SELECT objectid
+        FROM annee
+        WHERE annee.millesime = '01/01/2006')
+    WHEN e.valeur = 'PSDC99'
+      THEN (
+        SELECT objectid
+        FROM annee
+        WHERE annee.millesime = '01/01/1999')
+    WHEN e.valeur = 'PSDC90'
+      THEN (
+        SELECT objectid
+        FROM annee
+        WHERE annee.millesime = '01/01/1990')
+    WHEN e.valeur = 'PSDC82'
+      THEN (
+        SELECT objectid
+        FROM annee
+        WHERE annee.millesime = '01/01/1982')
+    WHEN e.valeur = 'PSDC75'
+      THEN (
+        SELECT objectid
+        FROM annee
+        WHERE annee.millesime = '01/01/1975')
+    WHEN e.valeur = 'PSDC68'
+      THEN (
+        SELECT objectid
+        FROM annee
+        WHERE annee.millesime = '01/01/1968')
+    WHEN e.valeur = 'PSDC62'
+      THEN (
+        SELECT objectid
+        FROM annee
+        WHERE annee.millesime = '01/01/1962')
+    WHEN e.valeur = 'PTOT54'
+      THEN (
+        SELECT objectid
+        FROM annee
+        WHERE annee.millesime = '01/01/1954')
+    WHEN e.valeur = 'PTOT36'
+      THEN (
+        SELECT objectid
+        FROM annee
+        WHERE annee.millesime = '01/01/1936')
+    WHEN e.valeur = 'PTOT1931'
+      THEN (
+        SELECT objectid
+        FROM annee
+        WHERE annee.millesime = '01/01/1931')
+    WHEN e.valeur = 'PTOT1926'
+      THEN (
+        SELECT objectid
+        FROM annee
+        WHERE annee.millesime = '01/01/1926')
+    WHEN e.valeur = 'PTOT1921'
+      THEN (
+        SELECT objectid
+        FROM annee
+        WHERE annee.millesime = '01/01/1921')
+    WHEN e.valeur = 'PTOT1911'
+      THEN (
+        SELECT objectid
+        FROM annee
+        WHERE annee.millesime = '01/01/1911')
+    WHEN e.valeur = 'PTOT1906'
+      THEN (
+        SELECT objectid
+        FROM annee
+        WHERE annee.millesime = '01/01/1906')
+    WHEN e.valeur = 'PTOT1901'
+      THEN (
+        SELECT objectid
+        FROM annee
+        WHERE annee.millesime = '01/01/1901')
+    WHEN e.valeur = 'PTOT1896'
+      THEN (
+        SELECT objectid
+        FROM annee
+        WHERE annee.millesime = '01/01/1896')
+    WHEN e.valeur = 'PTOT1891'
+      THEN (
+        SELECT objectid
+        FROM annee
+        WHERE annee.millesime = '01/01/1891')
+    WHEN e.valeur = 'PTOT1886'
+      THEN (
+        SELECT objectid
+        FROM annee
+        WHERE annee.millesime = '01/01/1886')
+    WHEN e.valeur = 'PTOT1881'
+      THEN (
+        SELECT objectid
+        FROM annee
+        WHERE annee.millesime = '01/01/1881')
+    WHEN e.valeur = 'PTOT1876'
+      THEN (
+        SELECT objectid
+        FROM annee
+        WHERE annee.millesime = '01/01/1876')
+    END AS metadonnee
 FROM
-    population p
-    INNER JOIN ta_code c ON p.codgeo = c.code
-    INNER JOIN ta_libelle cl ON c.fid_libelle = cl.objectid
-    INNER JOIN ta_libelle_long cll ON cl.fid_libelle_long = cll.objectid
-    INNER JOIN ta_famille_libelle cfl ON cll.objectid = cfl.fid_libelle_long
-    INNER JOIN ta_famille cf ON cfl.fid_famille = cf.objectid    
-    INNER JOIN ta_libelle_court lc ON p.recensement = lc.valeur
-    INNER JOIN ta_correspondance_libelle tc ON lc.objectid = tc.fid_libelle_court 
-    INNER JOIN ta_libelle l ON tc.fid_libelle = l.objectid
-    INNER JOIN ta_libelle_long ll ON l.fid_libelle_long = ll.objectid
-    INNER JOIN ta_famille_libelle fl ON ll.objectid = fl.fid_libelle_long
-    INNER JOIN ta_famille f ON fl.fid_famille = f.objectid
+    population b
+-- distinction des codes insee
+    INNER JOIN ta_code a ON b.codgeo = a.valeur
+    INNER JOIN ta_libelle c ON a.fid_libelle = c.objectid
+    INNER JOIN ta_libelle_long d ON c.fid_libelle_long = d.objectid
+-- distinction des codes du recensement
+    INNER JOIN ta_libelle_court e ON b.recensement = e.valeur
+    INNER JOIN ta_libelle_correspondance f ON e.objectid = f.fid_libelle_court 
+    INNER JOIN ta_libelle g ON f.fid_libelle = g.objectid
+    INNER JOIN ta_libelle_long h ON g.fid_libelle_long = h.objectid
+    INNER JOIN ta_famille_libelle i ON h.objectid = i.fid_libelle_long
+    INNER JOIN ta_famille j ON i.fid_famille = j.objectid
     WHERE
-    f.valeur = 'Recensement'
+    j.valeur = 'Recensement'
     AND
-    cll.valeur = 'code insee'
-    AND
-    cf.valeur = 'Identifiant de zone administrative'
+    d.valeur = 'code insee'
 ;
