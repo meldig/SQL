7b16c212bd4a9bc9054051ac5b0122acf70ed45e
romain-jault <57357284+romain-jault@users.noreply.github.com>
2020-07-03 09:01:09 +0200

    Update iris_nomenclature.sql
    Mise à jour des requêtes nécessaires à l'insertion des données Contours..IRIS.

--- integration/IRIS/iris_nomenclature.sql
+++ integration/IRIS/iris_nomenclature.sql
@@ -2,30 +2,30 @@
 Ensemble des requêtes utilisés pour insérer la nomenclature des données IRIS dans la base de données.
 */
 -- 1. Insertion de la source dans TA_SOURCE
-MERGE INTO ta_source s
+MERGE INTO ta_source a
 USING 
     (
-        SELECT 'Contours IRIS' AS nom, 'Contours...Iris®, coédition Insee et IGN, est la base de données de référence pour la diffusion infracommunale des résultats du recensement de la population par Iris, de précision décamétrique.' AS description FROM DUAL
-    ) temp
-ON (temp.nom = s.nom_source
-AND temp.description = s.description)
+        SELECT 'Contours...IRIS' AS nom_source, 'Contours...Iris®, coédition Insee et IGN, est la base de données de référence pour la diffusion infracommunale des résultats du recensement de la population par Iris, de précision décamétrique.' AS description FROM DUAL
+    ) b
+ON (a.nom_source = b.nom_source
+AND a.description = b.description)
 WHEN NOT MATCHED THEN
-INSERT (s.nom_source,s.description)
-VALUES (temp.nom,temp.description)
+INSERT (a.nom_source,a.description)
+VALUES (b.nom_source,b.description)
 ;
 
 
 -- 2. Insertion de la provenance de la données dans TA_PROVENANCE
-MERGE INTO ta_provenance p
+MERGE INTO ta_provenance a
 USING
     (
         SELECT 'https://geoservices.ign.fr/documentation/diffusion/telechargement-donnees-libres.html#contoursiris' AS url, 'Donnée à télécharger en libre accès' AS methode_acquisition FROM DUAL
-    ) temp
-ON (p.url = temp.url
-AND p.methode_acquisition = temp.methode_acquisition)
+    ) b
+ON (a.url = b.url
+AND a.methode_acquisition = b.methode_acquisition)
 WHEN NOT MATCHED THEN
-INSERT (p.url,p.methode_acquisition)
-VALUES(temp.url,temp.methode_acquisition)
+INSERT (a.url,a.methode_acquisition)
+VALUES(b.url,b.methode_acquisition)
 ;
 
 
@@ -34,13 +34,14 @@ VALUES(temp.url,temp.methode_acquisition)
 MERGE INTO ta_date_acquisition a
 USING
     (
-        SELECT TO_DATE('12/06/2012') AS DATE_ACQUISITION, TO_DATE('01/01/2018') AS MILLESIME,'rjault' AS OBTENTEUR FROM DUAL
-    ) temp
-ON (temp.date_acquisition = a.date_acquisition
-AND temp.millesime = a.millesime)
+        SELECT '12/06/2019' AS DATE_ACQUISITION, '03/09/2019' AS MILLESIME,'rjault' AS NOM_OBTENTEUR FROM DUAL
+    ) b
+ON (a.date_acquisition = b.date_acquisition
+AND a.millesime = b.millesime
+AND a.nom_obtenteur = b.nom_obtenteur)
 WHEN NOT MATCHED THEN
-INSERT (a.date_acquisition,a.millesime)
-VALUES (temp.date_acquisition,temp.millesime)
+INSERT (a.date_acquisition,a.millesime, a.nom_obtenteur)
+VALUES (b.date_acquisition, b.millesime, b.nom_obtenteur)
 ;
 
 
@@ -48,84 +49,114 @@ VALUES (temp.date_acquisition,temp.millesime)
 MERGE INTO ta_organisme a
 USING
     (
-        SELECT 'IGN' AS ACRONYME, 'Institut National de l''Information Géographique et Forestière' AS NOM_ORGANISME FROM DUAL
-    ) temp
-ON (a.acronyme = temp.acronyme)
+        SELECT 'IGN' AS ACRONYME, 'Institut National de l''Information Geographie et Forestiere' AS NOM_ORGANISME FROM DUAL
+        UNION
+        SELECT 'INSEE' AS ACRONYME, 'Institut National de la statistique et des études économiques' AS NOM_ORGANISME FROM DUAL
+    ) b
+ON (a.acronyme = b.acronyme)
 WHEN NOT MATCHED THEN
 INSERT (a.acronyme,a.nom_organisme)
-VALUES(temp.acronyme,temp.nom_organisme)
+VALUES(b.acronyme,b.nom_organisme)
 ;
 
-
 -- 5. Insertion de l'echelle d'utilisation dans la table TA_ECHELLE
-MERGE INTO ta_echelle e
+MERGE INTO ta_echelle a
 USING
     (
-        SELECT '1/1000000' AS ECHELLE FROM DUAL
-    )temp
-ON (e.echelle = temp.echelle)
+        SELECT '1 000 000' AS VALEUR FROM DUAL
+    )b
+ON (a.valeur = b.valeur)
 WHEN NOT MATCHED THEN
-INSERT (e.echelle)
-VALUES(temp.echelle)
+INSERT (a.valeur)
+VALUES(b.valeur)
 ;
 
 
 -- 6. Insertion des métadonnées dans la table TA_METADONNEE
 -- attention à l'insertion des dates, à mettre à jour suivant le millesime de la donnée à insérer
-MERGE INTO ta_metadonnee m
+MERGE INTO ta_metadonnee a
 USING
     (
         SELECT 
-            s.objectid AS fid_source,
-            a.objectid AS fid_acquisition,
-            p.objectid AS fid_provenance,
-            o.objectid AS fid_organisme,
-            e.objectid AS fid_echelle
+            a.objectid AS fid_source,
+            b.objectid AS fid_acquisition,
+            c.objectid AS fid_provenance,
+            d.objectid AS fid_echelle
         FROM
-            ta_source s,
-            ta_date_acquisition a,
-            ta_provenance p,
-            ta_organisme o,
-            ta_echelle e
+            ta_source a,
+            ta_date_acquisition b,
+            ta_provenance c,
+            ta_echelle d
         WHERE
-            s.nom_source = 'Contours IRIS'
+            a.nom_source = 'Contours...IRIS'
         AND
-            a.millesime IN ('01/01/2019')
+            b.millesime IN ('03/09/2019')
         AND
-            a.date_acquisition = '12/06/2020'
+            b.date_acquisition = '12/06/2019'
         AND
-            p.url = 'https://geoservices.ign.fr/documentation/diffusion/telechargement-donnees-libres.html#contoursiris'
+            c.url = 'https://geoservices.ign.fr/documentation/diffusion/telechargement-donnees-libres.html#contoursiris'
         AND
-            o.acronyme = 'IGN'
-        AND
-            e.echelle = '1/1000000'
+            d.valeur = '1 000 000'
     )temp
-ON (m.fid_source = temp.fid_source
-AND m.fid_acquisition = temp.fid_acquisition
-AND m.fid_provenance = temp.fid_provenance
-AND m.fid_organisme = temp.fid_organisme
-AND m.fid_echelle = temp.fid_echelle)
+ON (a.fid_source = temp.fid_source
+AND a.fid_acquisition = temp.fid_acquisition
+AND a.fid_provenance = temp.fid_provenance
+AND a.fid_echelle = temp.fid_echelle)
 WHEN NOT MATCHED THEN
-INSERT (m.fid_source, m.fid_acquisition, m.fid_provenance, m.fid_organisme, m.fid_echelle)
-VALUES (temp.fid_source, temp.fid_acquisition, temp.fid_provenance, temp.fid_organisme, temp.fid_echelle)
+INSERT (a.fid_source, a.fid_acquisition, a.fid_provenance, a.fid_echelle)
+VALUES (temp.fid_source, temp.fid_acquisition, temp.fid_provenance, temp.fid_echelle)
+;
+
+-- 7. Insertion des information dans la table ta_metadonnee_relation_organisme
+MERGE INTO ta_metadonnee_relation_organisme a
+USING
+    (
+        SELECT
+            a.objectid AS fid_metadonnee,
+            f.objectid AS fid_organisme
+        FROM
+            ta_metadonnee a
+        INNER JOIN ta_source b ON b.objectid = a.fid_source
+        INNER JOIN ta_date_acquisition c ON a.fid_acquisition = c.objectid
+        INNER JOIN ta_provenance d ON a.fid_provenance = d.objectid
+        INNER JOIN ta_echelle e ON a.fid_echelle = e.objectid,
+            ta_organisme f
+        WHERE
+            b.nom_source = 'Contours...IRIS'
+        AND
+            c.date_acquisition = '12/06/2019'
+        AND
+            c.millesime = '03/09/2019'
+        AND
+            d.url = 'https://geoservices.ign.fr/documentation/diffusion/telechargement-donnees-libres.html#contoursiris'
+        AND
+            e.valeur = '1 000 000'
+        AND
+            f.nom_organisme IN ('Institut National de l''Information Geographie et Forestiere','Institut National de la statistique et des études économiques')
+    )b
+ON(a.fid_metadonnee = b.fid_metadonnee
+AND a.fid_organisme = b.fid_organisme)
+WHEN NOT MATCHED THEN
+INSERT(a.fid_metadonnee, a.fid_organisme)
+VALUES(b.fid_metadonnee, b.fid_organisme)
 ;
 
 
--- 7. Insertion de la nomenclature dans la table TA_LIBELLE_COURT
-MERGE INTO ta_libelle_court tl
+-- 8. Insertion de la nomenclature dans la table TA_LIBELLE_COURT
+MERGE INTO ta_libelle_court a
 USING 
     (
     SELECT DISTINCT TYP_IRIS AS VALEUR FROM contours_iris
-    ) temp
-ON (temp.VALEUR = tl.VALEUR)
+    ) b
+ON (a.VALEUR = b.VALEUR)
 WHEN NOT MATCHED THEN
-INSERT (tl.VALEUR)
-VALUES (temp.VALEUR)
+INSERT (a.VALEUR)
+VALUES (b.VALEUR)
 ;
 
 
--- 8. Insertion de la nomenclature dans la table TA_LIBELLE LONG
-MERGE INTO ta_libelle_long t
+-- 9. Insertion de la nomenclature dans la table TA_LIBELLE LONG
+MERGE INTO ta_libelle_long a
 USING 
     (
     SELECT 'IRIS d''habitat:  leur population se situe en général entre 1 800 et 5 000 habitants. Ils sont homogènes quant au type d''habitat et leurs limites s''appuient sur les grandes coupures du tissu urbaIN (voies prINcipales, voies ferrées, cours d''eau, ...)' AS VALEUR FROM dual
@@ -137,35 +168,33 @@ USING
     SELECT 'Communes non découpées en IRIS' AS VALEUR FROM dual
     UNION
     SELECT 'code IRIS' AS VALEUR FROM dual
-    ) temp
-  ON (temp.VALEUR = t.VALEUR)
+    ) b
+  ON (a.VALEUR = b.VALEUR)
   WHEN NOT MATCHED THEN
-  INSERT (t.VALEUR)
-  VALUES (temp.VALEUR)
+  INSERT (a.VALEUR)
+  VALUES (b.VALEUR)
   ;
 
 
--- 9. Insertion des familles utilisée par les données IRIS dans la table TA_FAMILLE.
-MERGE INTO ta_famille f
+-- 10. Insertion des familles utilisée par les données IRIS dans la table TA_FAMILLE.
+MERGE INTO ta_famille a
 USING 
     (
     SELECT 'type de zone IRIS' AS VALEUR FROM dual
-    UNION
-    SELECT 'Identifiant de zone administrative' AS VALEUR FROM dual
-    ) temp
-ON (temp.VALEUR = f.VALEUR)
+    ) b
+ON (a.VALEUR = b.VALEUR)
 WHEN NOT MATCHED THEN
-INSERT (f.VALEUR)
-VALUES (temp.VALEUR);
+INSERT (a.VALEUR)
+VALUES (b.VALEUR);
 
 
--- 10. Insertion des correspondances famille libelle dans TA_FAMILLE_LIBELLE;
-MERGE INTO ta_famille_libelle fl
+-- 11. Insertion des correspondances famille libelle dans TA_FAMILLE_LIBELLE;
+MERGE INTO ta_famille_libelle a
 USING 
     (
     SELECT
-        a.objectid AS objectid_famille,
-        b.objectid AS objectid_libelle_long
+        a.objectid AS fid_famille,
+        b.objectid AS fid_libelle_long
     FROM
         ta_famille a,
         ta_libelle_long b
@@ -173,17 +202,16 @@ USING
         a.VALEUR = 'type de zone IRIS' AND b.VALEUR = 'IRIS d''habitat:  leur population se situe en général entre 1 800 et 5 000 habitants. Ils sont homogènes quant au type d''habitat et leurs limites s''appuient sur les grandes coupures du tissu urbaIN (voies prINcipales, voies ferrées, cours d''eau, ...)' OR
         a.VALEUR = 'type de zone IRIS' AND b.VALEUR = 'IRIS divers: il s''agit de grandes zones spécifiques peu habitées et ayant une superficie importante (parcs de loisirs, zones portuaires, forêts, ....' OR
         a.VALEUR = 'type de zone IRIS' AND b.VALEUR = 'IRIS d''activité: ils regroupent environ 1 000 salariés et comptent au moINs deux fois plus d''emplois salariés que de population résidente' OR
-        a.VALEUR = 'type de zone IRIS' AND b.VALEUR = 'Communes non découpées en IRIS' OR
-        a.VALEUR = 'Identifiant de zone administrative' AND b.VALEUR = 'code IRIS'
-    ) temp
-ON (temp.objectid_famille = fl.fid_famille
-AND temp.objectid_libelle_long = fl.fid_libelle_long)
+        a.VALEUR = 'type de zone IRIS' AND b.VALEUR = 'Communes non découpées en IRIS'
+    ) b
+ON (a.fid_famille = b.fid_famille
+AND a.fid_libelle_long = b.fid_libelle_long)
 WHEN NOT MATCHED THEN
-INSERT (fl.fid_famille,fl.fid_libelle_long)
-VALUES (temp.objectid_famille,temp.objectid_libelle_long);
+INSERT (a.fid_famille,a.fid_libelle_long)
+VALUES (b.fid_famille,b.fid_libelle_long);
 
 
--- 11. Création de la table temporaire fusion_nomenclature_iris
+-- 12. Création de la table temporaire fusion_nomenclature_iris
 CREATE GLOBAL TEMPORARY TABLE fusion_nomenclature_iris AS
 (SELECT
     a.objectid AS objectid,
@@ -208,11 +236,11 @@ WHERE
 );
 
 
--- 12. Insertion des données dans la table temporaire fusion_nomenclature_iris
+-- 13. Insertion des données dans la table temporaire fusion_nomenclature_iris
 INSERT INTO fusion_nomenclature_iris
 SELECT
 -- Attention à la séquence utilisée
-    ISEQ$$_78716.nextval as objectid,
+    ISEQ$$_1004437.nextval as objectid,
 -- Attention à la séquence utilisée
     b.objectid AS fid_libelle_long,
     b.VALEUR AS libelle_long,
@@ -234,40 +262,40 @@ WHERE
 ;
 
 
--- 13. Insertion des données dans ta_libelle
-MERGE INTO ta_libelle l
+-- 14. Insertion des données dans ta_libelle
+MERGE INTO ta_libelle a
 USING
     (
     SELECT
         objectid, 
         fid_libelle_long
     FROM fusion_nomenclature_iris
-    ) temp
-ON (temp.objectid = l.objectid
-AND temp.fid_libelle_long = l.fid_libelle_long)
+    ) b
+ON (a.objectid = b.objectid
+AND a.fid_libelle_long = b.fid_libelle_long)
 WHEN NOT MATCHED THEN
-INSERT (l.objectid,l.fid_libelle_long)
-VALUES (temp.objectid,temp.fid_libelle_long)
+INSERT (a.objectid,a.fid_libelle_long)
+VALUES (b.objectid,b.fid_libelle_long)
 ;
 
 
--- 14. Insertion des données dans ta_correspondance_libelle
-MERGE INTO ta_correspondance_libelle tc
+-- 15. Insertion des données dans ta_correspondance_libelle
+MERGE INTO ta_correspondance_libelle a
 USING
     (
     SELECT
-        objectid, 
+        objectid AS FID_LIBELLE, 
         fid_libelle_court
     FROM fusion_nomenclature_iris
-    ) temp
-ON (temp.objectid = tc.fid_libelle
-AND temp.fid_libelle_court = tc.fid_libelle_court)
+    ) b
+ON (a.fid_libelle = b.fid_libelle
+AND a.fid_libelle_court = b.fid_libelle_court)
 WHEN NOT MATCHED THEN
-INSERT (tc.fid_libelle,tc.fid_libelle_court)
-VALUES (temp.objectid,temp.fid_libelle_court)
+INSERT (a.fid_libelle,a.fid_libelle_court)
+VALUES (b.fid_libelle,b.fid_libelle_court)
 ;
 
 
--- 15. Suppression des tables et des vues utilisés seulement pour l'insertion de la nomenclature.
--- 15.1. Suppression de la table temporaire fusion_nomenclature_iris
+-- 16. Suppression des tables et des vues utilisés seulement pour l'insertion de la nomenclature.
+-- 16. Suppression de la table temporaire fusion_nomenclature_iris
 DROP TABLE fusion_nomenclature_iris CASCADE CONSTRAINTS PURGE;
