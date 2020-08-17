2e102bafddb6d92dc93d49dbcae481fa72f62ce4
BaptisteJacq <50438172+BaptisteJacq@users.noreply.github.com>
2020-08-17 11:28:44 +0200

    Correction GRANT SELECT sur ta_organisme

--- schema/structure commune/creation_ta_organisme.sql
+++ schema/structure commune/creation_ta_organisme.sql
@@ -22,4 +22,4 @@ PRIMARY KEY("OBJECTID")
 USING INDEX TABLESPACE "G_ADT_INDX";
 
 -- 6. Affectation du droit de sélection sur les objets de la table aux administrateurs
-GRANT SELECT ON g_geo.ta_organisme TO G_ADT_DSIG_ADM;
+GRANT SELECT ON g_geo.ta_organisme TO G_ADMIN_SIG;
