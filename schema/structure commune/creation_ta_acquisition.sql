4efe5dcbed5d9298a0ebc5f171782cb31a8fe62f
BaptisteJacq <50438172+BaptisteJacq@users.noreply.github.com>
2020-08-20 21:24:44 +0200

    Mise a jour du ddl des tables (#96)
    * Rectification du code DDL de TA_COMMUNE.
    
    Rectification du code DDL de TA_COMMUNE.
    
    * Rectification du code DDL de TA_FAMILLE
    
    * Rectification du code DDL de TA_FAMILLE_LIBELLE
    
    * Rectification du code DDL de TA_LIBELLE
    
    * Create creation_ta_libelle_long.sql
    
    L'idée étant d'avoir un fichier spécifique au code DDL de la table TA_LIBELLE_LONG.
    
    * Rectification du code DDL de TA_ORGANISME
    
    * Rectification du code DDL de TA_SOURCE
    
    * Create creation_ta_metadonnee.sql
    
    * Suppression des tables qui ne sont plus utilisées
    
    * Rectification du code DDL de VM_UNITE_TERRITORIALE
    
    * Update creation_vm_territoires.sql
    
    * Rectification du code DDL des vues des communes MEL
    
    * Create creation_ta_identifiant_commune.sql
    
    * Create creation_ta_identifiant_zone_administrative.sql
    
    * Correction nom de table + grant select
    
    * Correction GRANT SELECT sur ta_commune
    
    * Correction GRANT SELECT sur ta_famille
    
    * Correction GRANT SELECT sur ta_famille_libelles
    
    * Correction GRANT SELECT sur ta_libelle
    
    * Correction GRANT SELECT sur ta_libelle_long
    
    * Correction GRANT SELECT sur metadonnees
    
    * Correction GRANT SELECT sur ta_organisme
    
    * Correction GRANT SELECT sur ta_source
    
    * correction de la preview ta_date_acquisition
    
    * Correction de la preview ta_commune

--- schema/structure commune/creation_ta_acquisition.sql
+++ schema/structure commune/creation_ta_acquisition.sql
@@ -1,44 +1,31 @@
 /*
-La table ta_source permet de rassembler toutes les données sources provenant d'une source extérieure à la MEL.
+Table recensant les dates d'acquisition, de millésime et du nom de l'obtenteur de chaque donnée source extérieure à la MEL.
 */
 
 -- 1. Création de la table ta_source
-CREATE TABLE ta_acquisition(
-    objectid NUMBER(38,0),
+CREATE TABLE ta_date_acquisition(
+    objectid NUMBER(38,0) GENERATED ALWAYS AS IDENTITY,
     date_acquisition DATE,
     millesime DATE,
     nom_obtenteur VARCHAR2(200)
 );
 
 -- 2. Création des commentaires sur la table et les champs
-COMMENT ON TABLE ta_acquisition IS 'Table recensant les dates d''acquisition, de millésime et du nom de l''obtenteur de chaque donnée source extérieure à la MEL.';
-COMMENT ON COLUMN geo.ta_acquisition.objectid IS 'Identifiant de chaque objet de la table.';
-COMMENT ON COLUMN geo.ta_acquisition.date_acquisition IS 'Date d''importation de la donnée dans la table - DD/MM/AAAA.';
-COMMENT ON COLUMN geo.ta_acquisition.millesime IS 'Date de création de la donnée - MM/AAAA.';
-COMMENT ON COLUMN geo.ta_acquisition.nom_obtenteur IS 'Nom de la personne ayant inséré la donnée source dans la base.';
+COMMENT ON TABLE ta_date_acquisition IS 'Table recensant les dates d''acquisition, de millésime et du nom de l''obtenteur de chaque donnée source extérieure à la MEL.';
+COMMENT ON COLUMN geo.ta_date_acquisition.objectid IS 'Identifiant de chaque objet de la table.';
+COMMENT ON COLUMN geo.ta_date_acquisition.date_acquisition IS 'Date d''importation de la donnée dans la table - DD/MM/AAAA.';
+COMMENT ON COLUMN geo.ta_date_acquisition.millesime IS 'Date de création de la donnée - MM/AAAA.';
+COMMENT ON COLUMN geo.ta_date_acquisition.nom_obtenteur IS 'Nom de la personne ayant inséré la donnée source dans la base.';
 
 -- 3. Création de la clé primaire
-ALTER TABLE ta_acquisition 
-ADD CONSTRAINT ta_acquisition_PK 
+ALTER TABLE ta_date_acquisition 
+ADD CONSTRAINT ta_date_acquisition_PK 
 PRIMARY KEY("OBJECTID") 
 USING INDEX TABLESPACE "INDX_GEO";
 
--- 4. Création de la séquence d'auto-incrémentation
-CREATE SEQUENCE SEQ_ta_acquisition
-START WITH 1 INCREMENT BY 1;
+-- 5. Création du déclencheur d'enregistrement de la date d'insertion de la donnée et du nom de la personne ayant fait cet import, dans la table ta_date_acquisition.
 
--- 5. Création du déclencheur de la séquence permettant d'avoir une PK auto-incrémentée
-CREATE OR REPLACE TRIGGER BEF_ta_acquisition
-BEFORE INSERT ON ta_acquisition
-FOR EACH ROW
-BEGIN
-    	:new.objectid := SEQ_ta_acquisition.nextval;
-    END IF;
-END;
-
--- 8. Création du déclencheur d'enregistrement de la date d'insertion de la donnée et du nom de la personne ayant fait cet import, dans la table ta_acquisition.
-
-CREATE OR REPLACE TRIGGER ta_acquisition
+CREATE OR REPLACE TRIGGER ta_date_acquisition
 BEFORE INSERT ON ta_source
 FOR EACH ROW
 DECLARE
@@ -47,10 +34,10 @@ DECLARE
 BEGIN
 	select sys_context('USERENV','OS_USER') into username from dual;
 	IF INSERTING THEN
-		ta_acquisition.date_acquisition := sysdate;
-		ta_acquisition.nom_obtenteur := username; 
+		ta_date_acquisition.date_acquisition := sysdate;
+		ta_date_acquisition.nom_obtenteur := username; 
 	END IF;
 
 END;
 -- 8. Affectation du droit de sélection sur les objets de la table aux administrateurs
-GRANT SELECT ON geo.ta_acquisition TO G_ADT_DSIG_ADM;
+GRANT SELECT ON geo.ta_date_acquisition TO G_ADMIN_SIG;
--- schema/structure commune/creation_ta_organisme.sql
+++ schema/structure commune/creation_ta_organisme.sql
@@ -3,34 +3,23 @@ La table ta_organisme recense tous les organismes créateurs de données desquel
 */
 
 -- 1. Création de la table ta_organisme
-CREATE TABLE ta_organisme(
-    objectid NUMBER(38,0),
+CREATE TABLE g_geo.ta_organisme(
+    objectid NUMBER(38,0) GENERATED ALWAYS AS IDENTITY,
     acronyme VARCHAR2(50),
     nom_organisme VARCHAR2(2000)
 );
 
 -- 2. Création des commentaires sur la table et les champs
-COMMENT ON TABLE ta_organisme IS 'Table rassemblant tous les organismes créateurs des données source utilisées par la MEL.';
-COMMENT ON COLUMN geo.ta_organisme.objectid IS 'Identifiant de chaque objet de la table.';
-COMMENT ON COLUMN geo.ta_organisme.nom_organisme IS 'Nom de l''organisme créateur des données sources utilisées par la MEL';
+COMMENT ON TABLE g_geo.ta_organisme IS 'Table rassemblant tous les organismes créateurs des données source utilisées par la MEL.';
+COMMENT ON COLUMN g_geo.ta_organisme.objectid IS 'Identifiant de chaque objet de la table.';
+COMMENT ON COLUMN g_geo.ta_organisme.acronyme IS 'Sigle correspondant à l''organisme créateur de la donnée source';
+COMMENT ON COLUMN g_geo.ta_organisme.nom_organisme IS 'Nom de l''organisme créateur des données sources utilisées par la MEL';
 
 -- 3. Création de la clé primaire
-ALTER TABLE ta_organisme 
+ALTER TABLE g_geo.ta_organisme 
 ADD CONSTRAINT ta_organisme_PK 
 PRIMARY KEY("OBJECTID") 
-USING INDEX TABLESPACE "INDX_GEO";
-
--- 4. Création de la séquence d'auto-incrémentation
-CREATE SEQUENCE SEQ_ta_organisme
-START WITH 1 INCREMENT BY 1;
-
--- 5. Création du déclencheur de la séquence permettant d'avoir une PK auto-incrémentée
-CREATE OR REPLACE TRIGGER BEF_ta_organisme
-BEFORE INSERT ON ta_organisme
-FOR EACH ROW
-BEGIN
-    :new.objectid := SEQ_ta_organisme.nextval;
-END;
+USING INDEX TABLESPACE "G_ADT_INDX";
 
 -- 6. Affectation du droit de sélection sur les objets de la table aux administrateurs
-GRANT SELECT ON geo.ta_organisme TO G_ADT_DSIG_ADM;
+GRANT SELECT ON g_geo.ta_organisme TO G_ADMIN_SIG;
