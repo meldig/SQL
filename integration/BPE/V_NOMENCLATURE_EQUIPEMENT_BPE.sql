32717ff7ed281dbfb613e3ec73a9e91fe6ffdf97
romain-jault <57357284+romain-jault@users.noreply.github.com>
2020-09-18 12:07:27 +0200

    Integration bpe (#89)
    Mise à jour du fichier creation structure bpe
    
    Mise à jour de la documentation sur la méthodologie mise en place pour l'insertion des données BPE en base
    
    Mise à jour du fichier insertion_nomenclature_bpe en base
    
    Mise à jour du fichier normalisation des bpe en base.
    
    Requete de création de la vue materialisée des bpe sur le territoire de la MEL.
    
    Requete de creation de la vue restituant la nomenclature des types d'équipements contenus par la base permanente des equipements.
    
    Requete restituant les différentes variables caractérisant les équipements BPE.
    
    Mise à jour du fichier de normalisation des données BPE suite à l'insertion en base des BPE localisées dans les Hauts-de-France.
    
    fichier correctif pour Olivier
    
    rennomage du fichier VM_BPE -> admin_bpe
    
    Co-authored-by: BaptisteJacq <50438172+BaptisteJacq@users.noreply.github.com>

---
+++ integration/BPE/V_NOMENCLATURE_EQUIPEMENT_BPE.sql
@@ -0,0 +1,67 @@
+-- relation bpe
+CREATE OR REPLACE VIEW V_NOMENCLATURE_EQUIPEMENT_BPE AS
+WITH cte1 AS
+	(
+	SELECT 
+	    ggp.fid_libelle_parent AS fid_libelle_niv_0,
+	    gp.fid_libelle_parent AS fid_libelle_niv_1,
+	    p.fid_libelle_parent AS fid_libelle_niv_2,
+	    p.fid_libelle_fils AS fid_libelle_niv_3
+	from
+		ta_libelle_relation ggp
+	INNER JOIN ta_libelle_relation gp ON ggp.fid_libelle_fils = gp.fid_libelle_parent
+	INNER JOIN ta_libelle_relation p ON gp.fid_libelle_fils = p.fid_libelle_parent
+	)
+	SELECT
+		cte1.fid_libelle_niv_0,
+		tlc_niv_0.valeur AS libelle_court_niv_0,
+		tll_niv_0.valeur AS libelle_long_niv_0,
+		cte1.fid_libelle_niv_1,
+		tlc_niv_1.valeur AS libelle_court_niv_1,
+		tll_niv_1.valeur AS libelle_long_niv_1,		
+		cte1.fid_libelle_niv_2,
+		concat(tlc_niv_1.valeur,tlc_niv_2.valeur) AS libelle_court_niv_2,
+		tll_niv_2.valeur AS libelle_long_niv_2,
+		cte1.fid_libelle_niv_3,
+		concat(tlc_niv_1.valeur,concat(tlc_niv_2.valeur,tlc_niv_3.valeur)) AS libelle_court_niv_3,
+		tll_niv_3.valeur AS libelle_long_niv_3
+	FROM
+		cte1 
+	INNER JOIN ta_libelle_correspondance tc_niv_0 ON cte1.fid_libelle_niv_0 = tc_niv_0.fid_libelle
+	INNER JOIN ta_libelle_correspondance tc_niv_1 ON cte1.fid_libelle_niv_1 = tc_niv_1.fid_libelle
+	INNER JOIN ta_libelle_correspondance tc_niv_2 ON cte1.fid_libelle_niv_2 = tc_niv_2.fid_libelle
+	INNER JOIN ta_libelle_correspondance tc_niv_3 ON cte1.fid_libelle_niv_3 = tc_niv_3.fid_libelle
+	INNER JOIN ta_libelle_court tlc_niv_0 ON tc_niv_0.fid_libelle_court = tlc_niv_0.objectid
+	INNER JOIN ta_libelle_court tlc_niv_1 ON tc_niv_1.fid_libelle_court = tlc_niv_1.objectid
+	INNER JOIN ta_libelle_court tlc_niv_2 ON tc_niv_2.fid_libelle_court = tlc_niv_2.objectid
+	INNER JOIN ta_libelle_court tlc_niv_3 ON tc_niv_3.fid_libelle_court = tlc_niv_3.objectid
+	INNER JOIN ta_libelle tl_niv_0 ON cte1.fid_libelle_niv_0 = tl_niv_0.objectid
+	INNER JOIN ta_libelle tl_niv_1 ON cte1.fid_libelle_niv_1 = tl_niv_1.objectid
+	INNER JOIN ta_libelle tl_niv_2 ON cte1.fid_libelle_niv_2 = tl_niv_2.objectid
+	INNER JOIN ta_libelle tl_niv_3 ON cte1.fid_libelle_niv_3 = tl_niv_3.objectid
+	INNER JOIN ta_libelle_long tll_niv_0 ON tl_niv_1.fid_libelle_long = tll_niv_0.objectid
+	INNER JOIN ta_libelle_long tll_niv_1 ON tl_niv_1.fid_libelle_long = tll_niv_1.objectid
+	INNER JOIN ta_libelle_long tll_niv_2 ON tl_niv_2.fid_libelle_long = tll_niv_2.objectid
+	INNER JOIN ta_libelle_long tll_niv_3 ON tl_niv_3.fid_libelle_long = tll_niv_3.objectid
+	INNER JOIN ta_famille_libelle tfl_niv_0 ON tfl_niv_0.fid_libelle_long = tll_niv_0.objectid    
+	INNER JOIN ta_famille_libelle tfl_niv_1 ON tfl_niv_1.fid_libelle_long = tll_niv_1.objectid
+	INNER JOIN ta_famille_libelle tfl_niv_2 ON tfl_niv_2.fid_libelle_long = tll_niv_2.objectid
+	INNER JOIN ta_famille_libelle tfl_niv_3 ON tfl_niv_3.fid_libelle_long = tll_niv_3.objectid
+	INNER JOIN ta_famille tf_niv_0 ON tfl_niv_0.fid_famille = tf_niv_0.objectid
+	INNER JOIN ta_famille tf_niv_1 ON tfl_niv_1.fid_famille = tf_niv_1.objectid
+	INNER JOIN ta_famille tf_niv_2 ON tfl_niv_2.fid_famille = tf_niv_2.objectid
+	INNER JOIN ta_famille tf_niv_3 ON tfl_niv_3.fid_famille = tf_niv_3.objectid
+WHERE
+    tf_niv_0.valeur = 'BPE'
+    AND
+    tf_niv_1.valeur = 'BPE'
+    AND
+    tf_niv_2.valeur = 'BPE'
+    AND
+    tf_niv_3.valeur = 'BPE'
+ORDER BY
+	libelle_court_niv_0,
+	libelle_court_niv_1,
+	libelle_court_niv_2,
+	libelle_court_niv_3
+;
