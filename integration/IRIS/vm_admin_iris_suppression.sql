748916fbd9c0e2581f35e26b420e4e0641fc2554
romain-jault <57357284+romain-jault@users.noreply.github.com>
2020-11-18 15:44:28 +0100

    Update vm_admin_iris_suppression.sql
    Mise à jour de la numérotation. Le code est inchangé.

--- integration/IRIS/vm_admin_iris_suppression.sql
+++ integration/IRIS/vm_admin_iris_suppression.sql
@@ -2,13 +2,14 @@
 Requêtes nécessaires à la suppression des table temporaire nécessaire à la création de la vue matérialisée G_REFERENTIEL.ADMIN_IRIS.
 */
 
--- 9. Suppression des tables temporaire
--- 9.1
+-- 1. Suppression des tables temporaire.
+-- 1.1. Suppresion de la table temporaire G_REFERENTIEL.TEMP_COMMUNES_VM.
 DROP TABLE G_REFERENTIEL.TEMP_COMMUNES_VM CASCADE CONSTRAINTS;
+-- 1.1.1. Suppression des métadonnées de la table temporaire G_REFERENTIEL.TEMP_COMMUNES_VM.
 DELETE FROM USER_SDO_GEOM_METADATA WHERE table_name = 'TEMP_COMMUNES_VM';
 
--- 9.2
+-- 1.2. Suppression de la table temporaire G_REFERENTIEL.TEMP_COMMUNES_SURFACES.
 DROP TABLE G_REFERENTIEL.TEMP_COMMUNES_SURFACES CASCADE CONSTRAINTS;
 
--- 9.3
+-- 1.3. Suppression de la table temporaire G_REFERENTIEL.TEMP_COMMUNES_SURFACES_MAX.
 DROP TABLE G_REFERENTIEL.TEMP_COMMUNES_SURFACES_MAX CASCADE CONSTRAINTS;
