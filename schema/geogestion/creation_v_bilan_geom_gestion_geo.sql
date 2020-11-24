12fbb353bedf92046857996beb3ad3a8a1847bd7
BaptisteJacq <50438172+BaptisteJacq@users.noreply.github.com>
2020-11-24 15:53:50 +0100

    rajout des comentaires de la vue v_bilan_geom_gestion_geo

--- schema/geogestion/creation_v_bilan_geom_gestion_geo.sql
+++ schema/geogestion/creation_v_bilan_geom_gestion_geo.sql
@@ -2,7 +2,7 @@
 Etat des lieux des tables utilisées dans GestionGeo.
 */
 
--- Requête permettant de connaître les types de géométries par table (et leur nombre par GEO_ON_VALIDE), avec le nom de leur index, son tablespace, son statut et ses paramètres
+-- 1. Vue permettant de connaître les types de géométries par table (et leur nombre par GEO_ON_VALIDE), avec le nom de leur index, son tablespace, son statut et ses paramètres
 CREATE OR REPLACE FORCE VIEW geo.v_bilan_geom_gestion_geo(
     OBJECTID,
     OWNER,
@@ -119,3 +119,16 @@ ORDER BY
     TABLE_NAME,
     Type_geometrie,
     nb_objets;
+
+-- 2. Création des commentaires de la vue
+COMMENT ON TABLE V_BILAN_GEOM_GESTION_GEO IS 'Vue évaluant les types de géométries (par GEO_ON_VALIDE) et le nombre d''objets/géométries pour les tables TA_SUR_TOPO_G, TA_LIG_TOPO_G, TA_POINT_TOPO_G avec leur nom d''index spatial, son statut et ses paramètres.';
+COMMENT ON COLUMN V_BILAN_GEOM_GESTION_GEO.OBJECTID IS 'Clé primaire - cet identifiant n''a aucune autre utilité que celle de distinguer les entités de la vue.';
+COMMENT ON COLUMN V_BILAN_GEOM_GESTION_GEO.OWNER IS 'Nom du schéma dans lequel se trouve les tables.';
+COMMENT ON COLUMN V_BILAN_GEOM_GESTION_GEO.TABLE_NAME IS 'Nom des tables contenant les géométries à évaluer.';
+COMMENT ON COLUMN V_BILAN_GEOM_GESTION_GEO.TYPE_GEOMETRIE IS 'Types de géométries présentes dans les tables.';
+COMMENT ON COLUMN V_BILAN_GEOM_GESTION_GEO.NB_OBJETS IS 'Nombre d''objets par type de géométrie et par GEO_ON_VALIDE (0 ou 1).';
+COMMENT ON COLUMN V_BILAN_GEOM_GESTION_GEO.GEO_ON_VALIDE IS 'Objet tagué en tant que valide ou invalide (cette classification est propre aux tables de production et indépendante de la validité de la géométrie des objets.)';
+COMMENT ON COLUMN V_BILAN_GEOM_GESTION_GEO.INDEX_NAME IS 'Nom de l''index spatial utilisé sur le champ géométrique de chaque table.';
+COMMENT ON COLUMN V_BILAN_GEOM_GESTION_GEO.INDEX_TABLESPACE IS 'Nom du tablespace dans lequel se situe l''index spatial de chaque table.';
+COMMENT ON COLUMN V_BILAN_GEOM_GESTION_GEO.STATUS IS 'Statut valide/invalide de l''index spatial.';
+COMMENT ON COLUMN V_BILAN_GEOM_GESTION_GEO.PARAMETRES_INDEX IS 'Paramètres de chaque index spatial permettant, entre autre, de savoir quel type de géométrie peut contenir ce champ géométrique.';
