56bb971aaaf853752cf3fec64a2596536c4e5fe2
BaptisteJacq <50438172+BaptisteJacq@users.noreply.github.com>
2020-11-27 17:10:00 +0100

    Corrections
    - Suppression du champ density de la sélection ;
    - Rajout des tables '%_TOPO_IC' et 'TA_PIQUAGE_GPS' ;
    - MAJ de la liste de tables dont les stats doivent être mises à jour ;
    
    En relisant la documentation ci-dessous et ma requête je pense que ce qui a empêché la mise à jour des stats pour les tables gestiongeo seulement est que j'avais créé un doublon dans les identifiants de la liste.
    Documentation : https://blogs.oracle.com/optimizer/how-do-i-restrict-concurrent-statistics-gathering-to-a-small-set-of-tables-from-a-single-schema

--- analyse/maj_et_selection_statistiques_tables_gestiongeo.sql
+++ analyse/maj_et_selection_statistiques_tables_gestiongeo.sql
@@ -7,7 +7,7 @@ Attention cependant, la requête de mise à jour des statistiques risque de pren
 DECLARE
     filter_lst  DBMS_STATS.OBJECTTAB := DBMS_STATS.OBJECTTAB();
 BEGIN
-    filter_lst.extend(8);
+    filter_lst.extend(13);
     filter_lst(1).ownname := 'GEO';
     filter_lst(1).objname := 'TA_GG_%';
     filter_lst(2).ownname := 'GEO';
@@ -24,8 +24,16 @@ BEGIN
     filter_lst(7).objname := 'TA_LIG_TOPO_GPS';
     filter_lst(8).ownname := 'GEO';
     filter_lst(8).objname := 'TA_POINT_TOPO_GPS';
-    filter_lst(8).ownname := 'GEO';
-    filter_lst(8).objname := 'TA_DOSSIER_GPS';
+    filter_lst(9).ownname := 'GEO';
+    filter_lst(9).objname := 'TA_DOSSIER_GPS';
+    filter_lst(10).ownname := 'GEO';
+    filter_lst(10).objname := 'TA_LIG_TOPO_IC';
+    filter_lst(11).ownname := 'GEO';
+    filter_lst(11).objname := 'TA_LIG_TOPO_IC_2D';
+    filter_lst(12).ownname := 'GEO';
+    filter_lst(12).objname := 'TA_PIQUAGE_GPS';
+    filter_lst(13).ownname := 'GEO';
+    filter_lst(13).objname := 'TA_POINT_TOPO_IC';
     DBMS_STATS.GATHER_SCHEMA_STATS(ownname=>'GEO', obj_filter_list => filter_lst);
 END;
 
@@ -33,11 +41,10 @@ END;
 SELECT
     TABLE_NAME AS Nom_Table,
     COLUMN_NAME AS Nom_Champ,
-    DENSITY AS Densite_utilisation,
     DATA_TYPE AS Type_donnee,
     DATA_LENGTH AS longueur_Champ,
     DATA_PRECISION AS Precision_Champ,
-    NULLABLE AS Null,
+    NULLABLE,
     NUM_NULLS AS Nbr_Null,
     NUM_DISTINCT AS Nbr_Valeurs_Distinctes,
     LOW_VALUE AS Valeur_Minimum,
@@ -54,7 +61,11 @@ WHERE
         'TA_POINT_TOPO_F',
         'TA_LIG_TOPO_F',
         'TA_LIG_TOPO_GPS',
+        'TA_LIG_TOPO_IC',
+        'TA_LIG_TOPO_IC_2D',
         'TA_POINT_TOPO_GPS',
+        'TA_POINT_TOPO_IC',
+        'TA_PIQUAGE_GPS',
         'TA_DOSSIER_GPS',
         'TA_GG_APP_ADM_RIGHTS',
         'TA_GG_DOC',
