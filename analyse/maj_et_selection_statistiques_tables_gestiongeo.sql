bb4146c8c5aef336f034b2315e131c3cf76ff6f9
BaptisteJacq <50438172+BaptisteJacq@users.noreply.github.com>
2020-12-04 13:19:41 +0100

    Récupération commits gestiongeo (#137)
    * création de requêtes permettant la mise à jour des statistiques des tables gestiongeo et de leur sélection
    
    * correction_coquille
    
    - Suppression du champ density de la sélection ;
    - Rajout des tables '%_TOPO_IC' et 'TA_PIQUAGE_GPS' ;
    - MAJ de la liste de tables dont les stats doivent être mises à jour ;
    
    En relisant la documentation ci-dessous et ma requête je pense que ce qui a empêché la mise à jour des stats pour les tables gestiongeo seulement est que j'avais créé un doublon dans les identifiants de la liste.
    Documentation : https://blogs.oracle.com/optimizer/how-do-i-restrict-concurrent-statistics-gathering-to-a-small-set-of-tables-from-a-single-schema
    
    * Correction coquilles dans la condition de la sélection
    
    * Mise à jour des statistiques des tables avec celles que j'ai rajoutée
    
    * Création des statistiques des tables GestionGeo (#128)
    Ces statistiques ont été créées en utilisant la requête de sélection du fichier analyse/maj_et_selection_statistiques_tables_gestiongeo.sql de la Pull Request #127
    
    * Rajout de quelques tables + MAJ de commentaires de champs (#129)
    Rajout des tables :
    - TA_LIG_TOPO_IC ;
    - TA_LIG_TOPO_IC_2D ;
    - TA_POINT_TOPO_IC ;
    - TA_PIQUAGE_GPS
    
    Mise à jour des champs des tables suivantes :
    - TA_GG_EXTERIEUR ;
    - TA_GG_MEDIA ;
    
    * Documentation des fonctions dynmap de gestiongeo (#126)
    Documentation présentant l'outil gestiongeo et ses usages
    
    * Create documentation_tables_gestiongeo.mdown
    
    * correction_tableau_statistique
    
    * Update documentation_tables_gestiongeo.mdown
    
    * Update documentation_tables_gestiongeo.mdown
    
    * Create selection_objets_dependant_tables_gestiongeo.sql
    Les requête permettent de sélectionner :
    - les indexes ;
    - les contraintes ;
    - les déclencheurs ;
    - les vues dépendantes des tables ;
    - les fonctions ;
    
    * Rajout de la sélection des indexes des tables GestionGeo
    
    * Insertion des tables tableaux de tables
    Il manque PTTOPO et il faut revoir TA_POINT_TOPO_IC
    
    * Création d'un dossier contenant les images de l'analyse des tables
    
    * Création des tableaux de répartition des valeurs DOS_PRIORITE de 2019 à 2020
    
    * analyse de la répartition du DOS_PRIORITE de 2019 à 2020
    
    * coquilles
    
    * corrections suite aux commentaires
    
    Co-authored-by: romain-jault <57357284+romain-jault@users.noreply.github.com>
    Co-authored-by: JACQ Baptiste <bjacq@cudl-lille.fr>

---
+++ analyse/maj_et_selection_statistiques_tables_gestiongeo.sql
@@ -0,0 +1,79 @@
+/*
+La requête ci-dessous permet de mettre à jour les statistiques des tables GestionGeo, puis de les sélectionner.
+Attention cependant, la requête de mise à jour des statistiques risque de prendre beaucoup de temps et de ressources, il faudrait donc plutôt l'utiliser le soir après 17h00.
+*/
+
+-- Mise à jour des statistiques des tables GestionGeo
+DECLARE
+    filter_lst  DBMS_STATS.OBJECTTAB := DBMS_STATS.OBJECTTAB();
+BEGIN
+    filter_lst.extend(13);
+    filter_lst(1).ownname := 'GEO';
+    filter_lst(1).objname := 'TA_GG_%';
+    filter_lst(2).ownname := 'GEO';
+    filter_lst(2).objname := 'TA_SUR_TOPO_G';
+    filter_lst(3).ownname := 'GEO';
+    filter_lst(3).objname := 'TA_LIG_TOPO_G';
+    filter_lst(4).ownname := 'GEO';
+    filter_lst(4).objname := 'TA_POINT_TOPO_G';
+    filter_lst(5).ownname := 'GEO';
+    filter_lst(5).objname := 'TA_POINT_TOPO_F';
+    filter_lst(6).ownname := 'GEO';
+    filter_lst(6).objname := 'TA_LIG_TOPO_F';
+    filter_lst(7).ownname := 'GEO';
+    filter_lst(7).objname := 'TA_LIG_TOPO_GPS';
+    filter_lst(8).ownname := 'GEO';
+    filter_lst(8).objname := 'TA_POINT_TOPO_GPS';
+    filter_lst(9).ownname := 'GEO';
+    filter_lst(9).objname := 'TA_DOSSIER_GPS';
+    filter_lst(10).ownname := 'GEO';
+    filter_lst(10).objname := 'TA_LIG_TOPO_IC';
+    filter_lst(11).ownname := 'GEO';
+    filter_lst(11).objname := 'TA_LIG_TOPO_IC_2D';
+    filter_lst(12).ownname := 'GEO';
+    filter_lst(12).objname := 'TA_PIQUAGE_GPS';
+    filter_lst(13).ownname := 'GEO';
+    filter_lst(13).objname := 'TA_POINT_TOPO_IC';
+    DBMS_STATS.GATHER_SCHEMA_STATS(ownname=>'GEO', obj_filter_list => filter_lst);
+END;
+
+-- Sélection des statistiques des tables de GestionGeo
+SELECT
+    TABLE_NAME AS Nom_Table,
+    COLUMN_NAME AS Nom_Champ,
+    DATA_TYPE AS Type_donnee,
+    DATA_LENGTH AS longueur_Champ,
+    DATA_PRECISION AS Precision_Champ,
+    NULLABLE,
+    NUM_NULLS AS Nbr_Null,
+    NUM_DISTINCT AS Nbr_Valeurs_Distinctes,
+    LOW_VALUE AS Valeur_Minimum,
+    HIGH_VALUE AS Valeur_Maximale
+FROM 
+    USER_TAB_COLUMNS
+WHERE 
+    TABLE_NAME IN(
+        'TA_SUR_TOPO_G',
+        'TA_POINT_TOPO_G',
+        'TA_LIG_TOPO_G',
+        'TA_GG_APP_ADM_RIGHTS',
+        'TA_GG_DOC',
+        'TA_GG_DOSSIER',
+        'TA_GG_ETAT',
+        'TA_GG_EXTERIEUR',
+        'TA_GG_FAMILLE',
+        'TA_GG_FILES',
+        'TA_GG_FORMAT',
+        'TA_GG_GEO',
+        'TA_GG_MEDIA',
+        'TA_GG_POINT',
+        'TA_GG_SOURCE',
+        'TA_LIG_TOPO_GPS',
+        'TA_POINT_TOPO_GPS',
+        'TA_LIG_TOPO_F',
+        'TA_POINT_TOPO_F',
+        'TA_LIG_TOPO_IC',
+        'TA_POINT_TOPO_IC',
+        'TA_LIG_TOPO_IC_2D',
+        'TA_PIQUAGE_GPS'
+    );
