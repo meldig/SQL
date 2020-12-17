e0d5133cb942800abda48adb9eb11d13ea6c9b06
BaptisteJacq <50438172+BaptisteJacq@users.noreply.github.com>
2020-12-17 15:24:38 +0100

    Création du code DDL de GestionGeo + import des données dans Oracle 12 (#143)
    Création du code DDL de G_GESTIONGEO
    
    * Finalisation du code ddl avant le premier test
    
    * Correction du code DDL de G_GESTIONGEO après le test
    Ce code créé uniquement les tables avec leurs contraitnes et index.
    Le code ne contient pas les triggers.
    
    * Code Suppression des objets du schéma G_GESTIONGEO
    
    * Création .bat permettant de migrer les tables de oracle 11 vers oracle 12
    
    * Rajout du paramètrage du système d'encodage dans le lanceur du code DDL du schéma
    
    * maj du .bat d'import pendant le test
    - rajout du @echo off qui clarifie la lecture de l'utilisateur ;
    - correction coquilles ;
    
    * Correction code_ddl_gestiongeo
    - Changement du type ID_GEOM de NUMBER à IDENTITY BY DEFAULT ;
    - Suppression des champ CLASSE_DICT et SHORT_NUM des tables finales ;
    - Création du code permettant d'insérer les données des tables temporaires dans les tables finales ;
    
    * maj des fichiers suite aux test + ajout fichier de correction erreur de géométries/doublons de TA_GG_GEO
    
    * Correction du code suite au point avec Gaëlle
    il ne reste plus que deux dossiers à problème : 186500311 et 105530454
    
    * remplacement des noms de schéma + import correction des géométries
    
    - tous les noms de schéma sont changés par le nouveau ;
    - le code de correction/fusion/suppression des géométrie est fonctionnel. Il faut maintenant le tester sur les objets du schéma de prod (mais en dev) afin de voir si le code s'applique à ces données ou s'il y eu des changements depuis la descente de la PROD sur la DEV (en tous cas le code corrige tout en DEV)
    
    * rajout du backup dans le code de correction des erreurs
    
    * Corrections suite aux test dans le nouveau schéma
    
    * Correction + rectification coquilles TA_GG_DOSSIER
    Création de deux dossiers dans TA_GG_DOSSIER. Ces dossiers correspondent à des périmètres de TA_GG_GEO qui ne disposaient pas de dossier, mais qu'il faut apparemment garder (vu avec Gaëlle).
    
    Co-authored-by: JACQ Baptiste <bjacq@cudl-lille.fr>

---
+++ integration/G_GESTIONGEO/correction_donnees_gestiongeo.sql
@@ -0,0 +1,389 @@
+SET SERVEROUTPUT ON
+BEGIN
+SAVEPOINT POINT_SAUVEGARDE_CORRECTION_TA_GG_GEO;
+
+-- Modification dans TA_GEO_CORRECT_DOUBLONS d'un ID_GEOM qui n'est plus dans TA_GG_GEO   
+UPDATE GEO.TA_GEO_CORRECT_DOUBLONS a
+SET a.ID_GEOM = 9426
+WHERE a.objectid = 44;
+
+-- Suppression d'un doublon de DOS_NUM (130090239) dans TA_GEO_CORRECT_DOUBLONS
+DELETE 
+FROM GEO.TA_GEO_CORRECT_DOUBLONS a
+WHERE a.OBJECTID = 91;
+
+-- Insertion de dossiers manquants dans TA_GEO_CORRECT_DOUBLONS
+INSERT INTO GEO.TA_GEO_CORRECT_DOUBLONS(ID_GEOM, DOS_NUM, GEOM, ACTION, CLASSE_DICT)
+SELECT
+    a.ID_GEOM,
+    a.DOS_NUM,
+    a.GEOM,
+    'fusionner',
+    CLASSE_DICT
+FROM
+    GEO.TEMP_GEO_CORRECTION a
+WHERE
+    a.ID_GEOM IN(
+        2855,
+        3800,
+        3877,
+        5091,
+        5119,
+        5165,
+        5171,
+        5470,
+        5503,
+        5519,
+        5533,
+        5736,
+        5760,
+        5846,
+        6013,
+        8539,
+        8576,
+        28658,
+        28776
+    );
+
+-- Suppression des polygones à supprimer de TEMP_GEO_CORRECTION
+DELETE
+FROM 
+    GEO.TEMP_GEO_CORRECTION a
+WHERE
+    a.ID_GEOM IN(4547, 83, 85, 2496, 652, 12648);
+
+/* Correction des erreurs de géométrie dans TEMP_GEO_CORRECTION via SDO_UTIL.RECTIFY_GEOMETRY
+Rappel - les erreurs que cette fonction corrige sont :
+- 13349 : le polygone l'intersecte lui-même ;
+- 13356 : le polygone a des sommets redondants ;
+- 13367 : les anneaux intérieurs et/ou extérieurs ont une mauvaise orientation ;
+- Attention : un polygone peut être concerné par plusieurs erreurs et dans ce cas la correction d'une erreur peut aussi résoudre une ou plusieurs autres erreurs sur ce même polygone.
+*/
+
+-- Erreur 13343 : suppression des polygones qui sont manifestement des erreurs : 1. ce sont des lignes de type 2003 ; 2.leur suppression n'impacte pas les périmètres des dossiers 
+DELETE FROM GEO.TEMP_GEO_CORRECTION a
+WHERE
+    SUBSTR(SDO_GEOM.VALIDATE_GEOMETRY_WITH_CONTEXT(a.geom, 0.005), 0, 5) = '13343';
+    
+-- Erreur 13028 : correction du SDO_ELEM_INFO_ARRAY de chaque entité
+UPDATE GEO.TEMP_GEO_CORRECTION a
+SET a.geom.SDO_ELEM_INFO = MDSYS.SDO_ELEM_INFO_ARRAY(1,1003,1)
+WHERE
+ SDO_GEOM.VALIDATE_GEOMETRY_WITH_CONTEXT(a.GEOM, 0.005) = '13028';
+
+-- Erreur 13349
+UPDATE GEO.TEMP_GEO_CORRECTION a
+    SET a.GEOM = SDO_UTIL.RECTIFY_GEOMETRY(a.geom, 0.005)
+WHERE
+    SUBSTR(SDO_GEOM.VALIDATE_GEOMETRY_WITH_CONTEXT(a.geom, 0.005), 0, 5) = '13349';
+    
+-- Erreur 13356
+UPDATE GEO.TEMP_GEO_CORRECTION a
+    SET a.GEOM = SDO_UTIL.RECTIFY_GEOMETRY(a.geom, 0.005)
+WHERE
+    SUBSTR(SDO_GEOM.VALIDATE_GEOMETRY_WITH_CONTEXT(a.geom, 0.005), 0, 5) = '13356';
+    
+-- Erreur 13367
+UPDATE GEO.TEMP_GEO_CORRECTION a
+    SET a.GEOM = SDO_UTIL.RECTIFY_GEOMETRY(a.geom, 0.005)
+WHERE
+    SUBSTR(SDO_GEOM.VALIDATE_GEOMETRY_WITH_CONTEXT(a.geom, 0.005), 0, 5) = '13367';
+
+-- Fusion des polygones d'un même dossier
+MERGE INTO GEO.TEMP_GEO_CORRECTION a
+    USING(
+        WITH
+            C_1 AS(
+                SELECT
+                    a.ID_GEOM,
+                    b.ID_DOS,
+                    b.CLASSE_DICT,
+                    a.DOS_NUM
+                FROM
+                    GEO.TA_GEO_CORRECT_DOUBLONS a
+                    INNER JOIN GEO.TEMP_GEO_CORRECTION b ON b.ID_GEOM = a.ID_GEOM
+                WHERE
+                    a.ACTION IN('fusionnner',
+                                'fusionnjer',
+                                'fusionner',
+                                'Fusionner',
+                                'fusionneer',
+                                'fusiojnner',
+                                ' fusionner',
+                                'fusioonner'
+                                )
+                    OR a.DOS_NUM IN(
+                                        133500235,
+                                        85120572,
+                                        103600230,
+                                        112790021,
+                                        113430583,
+                                        163780748,
+                                        164260850,
+                                        161631099,
+                                        166110706,
+                                        166700676,
+                                        172990464,
+                                        181430299,
+                                        181630451,
+                                        173780331,
+                                        175120447,
+                                        175240403,
+                                        180440448,
+                                        185530481,
+                                        181930445,
+                                        183670438,
+                                        184100135,
+                                        184870312,
+                                        186500311,
+                                        153780097,
+                                        163780407,
+                                        161630711,
+                                        105530454
+                                    )
+            ),
+            C_2 AS (
+                SELECT
+                b.DOS_NUM,
+                SDO_AGGR_UNION(
+                    SDOAGGRTYPE(b.geom, 0.005)
+                ) AS GEOM
+            FROM
+                C_1 a,
+                GEO.TEMP_GEO_CORRECTION b
+            WHERE
+                b.DOS_NUM = a.DOS_NUM
+            GROUP BY
+                b.DOS_NUM
+            )
+            SELECT
+                a.ID_GEOM,
+                a.ID_DOS,
+                a.CLASSE_DICT,
+                a.DOS_NUM,
+                b.GEOM,
+                SDO_GEOM.VALIDATE_GEOMETRY_WITH_CONTEXT(b.GEOM, 0.005) AS STATUT
+            FROM
+                C_1 a,
+                C_2 b
+            WHERE
+                a.DOS_NUM = b.DOS_NUM
+    )t
+    ON (a.ID_GEOM = t.ID_GEOM AND t.STATUT = 'TRUE')
+WHEN MATCHED THEN
+    UPDATE
+        SET a.GEOM = t.GEOM;
+
+-- Suppression des entités ayant servi à la fusion
+DELETE
+FROM 
+    GEO.TEMP_GEO_CORRECTION a
+WHERE
+    a.DOS_NUM IN (
+                    SELECT 
+                        DOS_NUM
+                    FROM
+                        GEO.TA_GEO_CORRECT_DOUBLONS a
+                    WHERE
+                        a.ACTION IN(
+                            'fusionnner',
+                            'fusionnjer',
+                            'fusionner',
+                            'Fusionner',
+                            'fusionneer',
+                            'fusiojnner',
+                            ' fusionner',
+                            'fusioonner'
+                                )
+                    OR a.DOS_NUM IN(
+                                        133500235,
+                                        85120572,
+                                        103600230,
+                                        112790021,
+                                        113430583,
+                                        163780748,
+                                        164260850,
+                                        161631099,
+                                        166110706,
+                                        166700676,
+                                        172990464,
+                                        181430299,
+                                        181630451,
+                                        173780331,
+                                        175120447,
+                                        175240403,
+                                        180440448,
+                                        185530481,
+                                        181930445,
+                                        183670438,
+                                        184100135,
+                                        184870312,
+                                        186500311,
+                                        153780097,
+                                        163780407,
+                                        161630711,
+                                        105530454
+                                    )
+                        
+                )
+    AND a.ID_GEOM NOT IN (
+                            SELECT 
+                                ID_GEOM
+                            FROM
+                                GEO.TA_GEO_CORRECT_DOUBLONS a
+                            WHERE
+                                a.ACTION IN(
+                                    'fusionnner',
+	                                'fusionnjer',
+	                                'fusionner',
+	                                'Fusionner',
+	                                'fusionneer',
+	                                'fusiojnner',
+	                                ' fusionner',
+                                    'fusioonner'
+                                )
+                    OR a.DOS_NUM IN(
+                                        133500235,
+                                        85120572,
+                                        103600230,
+                                        112790021,
+                                        113430583,
+                                        163780748,
+                                        164260850,
+                                        161631099,
+                                        166110706,
+                                        166700676,
+                                        172990464,
+                                        181430299,
+                                        181630451,
+                                        173780331,
+                                        175120447,
+                                        175240403,
+                                        180440448,
+                                        185530481,
+                                        181930445,
+                                        183670438,
+                                        184100135,
+                                        184870312,
+                                        186500311,
+                                        153780097,
+                                        163780407,
+                                        161630711,
+                                        105530454
+                                    )
+                                
+                        )
+;
+
+
+-- Passage de certains dossiers/périmètres en clôturés
+-- Résultats attendus : 4 lignes éditées
+UPDATE GEO.TEMP_GEO_CORRECTION a
+    SET a.ETAT_ID = 9
+WHERE
+    a.DOS_NUM IN(
+                    SELECT b.DOS_NUM
+                    FROM
+                        GEO.TA_GG_GEO b
+                    WHERE
+                        b.DOS_NUM IN(
+                                        202860388,
+                                        203680443,
+                                        20320445,
+                                        202020446,
+                                        202520447
+                                    )
+                )
+;
+-- Résultats attendus : 4 lignes éditées
+UPDATE GEO.TA_GG_DOSSIER a
+    SET a.ETAT_ID = 9
+WHERE
+    a.DOS_NUM IN(
+                    SELECT b.DOS_NUM
+                    FROM
+                        GEO.TA_GG_DOSSIER b
+                    WHERE
+                        b.DOS_NUM IN(
+                                        202860388,
+                                        203680443,
+                                        20320445,
+                                        202020446,
+                                        202520447
+                                    )
+                )
+;
+/*
+-- Création de deux nouveaux dossiers dans TA_GG_DOSSIER. Ces dossiers correspondront aux périmètres présents dans TA_GG_GEO dont le DOS_NUM = 5332, mais ne disposant pas de dossier dans TA_GG_DOSSIER pour le moment.
+INSERT INTO G_GESTIONGEO.TA_GG_DOSSIER(SRC_ID,ETAT_ID,USER_ID,FAM_ID,DOS_DC,DOS_PRECISION,DOS_DMAJ,DOS_RQ,DOS_DT_FIN,DOS_PRIORITE,DOS_IDPERE,DOS_DT_DEB_TR,DOS_DT_FIN_TR,DOS_DT_CMD_SAI,DOS_INSEE,DOS_VOIE,DOS_MAO,DOS_ENTR,ORDER_ID,DOS_NUM,DOS_OLD_ID,DOS_DT_DEB_LEVE,DOS_DT_FIN_LEVE,DOS_DT_PREV, DOS_URL_FILE)
+SELECT
+    a.SRC_ID,
+    a.ETAT_ID,
+    a.USER_ID,
+    a.FAM_ID,
+    a.DOS_DC,
+    a.DOS_PRECISION,
+    a.DOS_DMAJ,
+    a.DOS_RQ,
+    a.DOS_DT_FIN,
+    a.DOS_PRIORITE,
+    a.DOS_IDPERE,
+    a.DOS_DT_DEB_TR,
+    a.DOS_DT_FIN_TR,
+    a.DOS_DT_CMD_SAI,
+    a.DOS_INSEE,
+    3501303,
+    a.DOS_MAO,
+    a.DOS_ENTR,
+    a.ORDER_ID,
+    136500142,
+    a.DOS_OLD_ID,
+    a.DOS_DT_DEB_LEVE,
+    a.DOS_DT_FIN_LEVE,
+    a.DOS_DT_PREV,
+    'RECOL/163500142_59350_rue_du_ballon/'
+FROM
+    G_GESTIONGEO.TA_GG_DOSSIER a
+WHERE
+    a.DOS_NUM = 163500137;
+
+INSERT INTO G_GESTIONGEO.TA_GG_DOSSIER(SRC_ID,ETAT_ID,USER_ID,FAM_ID,DOS_DC,DOS_PRECISION,DOS_DMAJ,DOS_RQ,DOS_DT_FIN,DOS_PRIORITE,DOS_IDPERE,DOS_DT_DEB_TR,DOS_DT_FIN_TR,DOS_DT_CMD_SAI,DOS_INSEE,DOS_VOIE,DOS_MAO,DOS_ENTR,ORDER_ID,DOS_NUM,DOS_OLD_ID,DOS_DT_DEB_LEVE,DOS_DT_FIN_LEVE,DOS_DT_PREV, DOS_URL_FILE)
+SELECT
+    a.SRC_ID,
+    a.ETAT_ID,
+    a.USER_ID,
+    a.FAM_ID,
+    a.DOS_DC,
+    a.DOS_PRECISION,
+    a.DOS_DMAJ,
+    a.DOS_RQ,
+    a.DOS_DT_FIN,
+    a.DOS_PRIORITE,
+    a.DOS_IDPERE,
+    a.DOS_DT_DEB_TR,
+    a.DOS_DT_FIN_TR,
+    a.DOS_DT_CMD_SAI,
+    a.DOS_INSEE,
+    a.DOS_VOIE,
+    a.DOS_MAO,
+    a.DOS_ENTR,
+    a.ORDER_ID,
+    136500169,  
+    a.DOS_OLD_ID,
+    a.DOS_DT_DEB_LEVE,
+    a.DOS_DT_FIN_LEVE,
+    a.DOS_DT_PREV,
+    'RECOL/163500169_59350_rue_de_la_communaute/'
+FROM
+    G_GESTIONGEO.TA_GG_DOSSIER a
+WHERE
+    a.DOS_NUM = 163500137;
+*/    
+COMMIT;
+-- En cas d'erreur une exception est levée et un rollback effectué, empêchant ainsi toute insertion de se faire et de retourner à l'état des tables précédent l'insertion.
+EXCEPTION
+    WHEN OTHERS THEN
+        DBMS_OUTPUT.PUT_LINE('L''erreur ' || SQLCODE || 'est survenue. Un rollback a été effectué : ' || SQLERRM(SQLCODE));
+        ROLLBACK TO POINT_SAUVEGARDE_CORRECTION_TA_GG_GEO;
+END;
+/
---
+++ integration/G_GESTIONGEO/import_des_tables_gestiongeo.bat
@@ -0,0 +1,37 @@
+@echo off
+:: utilisation de ogr2ogr pour exporter des tables de CUDL vers MULTIT
+:: 1. gestion des identifiants Oracle
+SET /p USER_D="Veuillez saisir l'utilisateur Oracle de destination: "
+SET /p USER_P="Veuillez saisir l'utilisateur Oracle de provenance: "
+SET /p MDP_D="Veuillez saisir le mot de passe de l'utilisateur Oracle de destination: "
+SET /p MDP_P="Veuillez saisir le mot de passe de l'utilisateur Oracle de provenance: "
+SET /p INSTANCE_D="Veuillez saisir l'instance Oracle de destination: "
+SET /p INSTANCE_P="Veuillez saisir l'instance Oracle de provenance: "
+
+:: 2. se mettre dans l'environnement QGIS
+cd C:\Program Files\QGIS 3.16\bin
+
+:: 3. Configurer le système d'encodage des caractères en UTF-8
+SET NLS_LANG=AMERICAN_AMERICA.AL32UTF8
+
+:: 4. Rediriger la variable PROJ_LIB vers le bon fichier proj.db afin qu'ogr2ogr trouve le bon scr
+setx PROJ_LIB "C:\Program Files\QGIS 3.16\share\proj"
+
+:: 5. commande ogr2ogr pour exporter les couches du schéma X@X vers le schéma X@X
+:: 5.1. table TA_GG_FAMILLE
+ogr2ogr.exe -f OCI OCI:%USER_D%/%MDP_D%@%INSTANCE_D% OCI:%USER_P%/%MDP_P%@%INSTANCE_P% -sql "SELECT * FROM GEO.TA_GG_FAMILLE" -nln TEMP_TA_GG_FAMILLE 
+
+:: 5.2. table TA_GG_ETAT
+ogr2ogr.exe -f OCI OCI:%USER_D%/%MDP_D%@%INSTANCE_D% OCI:%USER_P%/%MDP_P%@%INSTANCE_P% -sql "SELECT * FROM GEO.TA_GG_ETAT" -nln TEMP_TA_GG_ETAT
+
+:: 5.3. table TA_GG_SOURCE
+ogr2ogr.exe -f OCI OCI:%USER_D%/%MDP_D%@%INSTANCE_D% OCI:%USER_P%/%MDP_P%@%INSTANCE_P% -sql "SELECT * FROM GEO.TA_GG_SOURCE" -nln TEMP_TA_GG_SOURCE
+
+:: 5.4. table TA_GG_DOSSIER
+ogr2ogr.exe -f OCI OCI:%USER_D%/%MDP_D%@%INSTANCE_D% OCI:%USER_P%/%MDP_P%@%INSTANCE_P% -sql "SELECT * FROM GEO.TA_GG_DOSSIER" -nln TEMP_TA_GG_DOSSIER
+
+:: 5.5. table TA_GG_GEO
+ogr2ogr.exe -f OCI OCI:%USER_D%/%MDP_D%@%INSTANCE_D% OCI:%USER_P%/%MDP_P%@%INSTANCE_P% -sql "SELECT * FROM GEO.TA_GG_GEO" -nln TEMP_TA_GG_GEO -nlt MULTIPOLYGON -lco SRID=2154 -dim 2
+
+:: 6. MISE EN PAUSE
+PAUSE
