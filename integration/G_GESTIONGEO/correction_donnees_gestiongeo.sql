35d73b3a7215db782a49b6d757aac4fb7c25dba6
BaptisteJacq <50438172+BaptisteJacq@users.noreply.github.com>
2020-12-18 19:19:25 +0100

    Correction suite aux corrections en base
    Les tables TA_GG_GEO et TA_GG_DOSSIER sont désormais corrigées en prod et les données sont importées dans oracle 12c.

--- integration/G_GESTIONGEO/correction_donnees_gestiongeo.sql
+++ integration/G_GESTIONGEO/correction_donnees_gestiongeo.sql
@@ -2,105 +2,17 @@ SET SERVEROUTPUT ON
 BEGIN
 SAVEPOINT POINT_SAUVEGARDE_TA_GG_GEO;
 
--- Désactivation de la clé étrangère de TA_GG_GEO qui dispose de l'option ON DELETE CASCADE vers TEMP_DOSSIER_CORRECTION
-EXECUTE IMMEDIATE 'ALTER TABLE GEO.TEMP_GEO_CORRECTION DISABLE CONSTRAINT TMP_GEO_CORRECT_ID_DOS_FK';
+-- Désactivation de la clé étrangère de TA_GG_GEO qui dispose de l'option ON DELETE CASCADE vers TA_GG_DOSSIER
+EXECUTE IMMEDIATE 'ALTER TABLE GEO.TA_GG_GEO DISABLE CONSTRAINT TA_GG_GEO_ID_DOS_FK';
 
--- Modification dans TEMP_GEO_CORRECT_DOUBLONS d'un ID_GEOM qui n'est plus dans TA_GG_GEO   
-UPDATE GEO.TEMP_GEO_CORRECT_DOUBLONS a
-SET a.ID_GEOM = 9426
-WHERE a.objectid = 44;
-
--- Suppression d'un doublon de DOS_NUM (130090239) dans TEMP_GEO_CORRECT_DOUBLONS
-DELETE 
-FROM GEO.TEMP_GEO_CORRECT_DOUBLONS a
-WHERE a.OBJECTID = 91;
-
--- Insertion de dossiers manquants dans TEMP_GEO_CORRECT_DOUBLONS
-INSERT INTO GEO.TEMP_GEO_CORRECT_DOUBLONS(ID_GEOM, DOS_NUM, GEOM, ACTION)
-SELECT
-    a.ID_GEOM,
-    a.DOS_NUM,
-    a.GEOM,
-    'fusionner'
-FROM
-    GEO.TA_GG_GEO a
-WHERE
-    a.ID_GEOM IN(
-        2855,
-        3800,
-        3877,
-        5091,
-        5119,
-        5165,
-        5171,
-        5470,
-        5503,
-        5519,
-        5533,
-        5736,
-        5760,
-        5846,
-        6013,
-        8539,
-        8576,
-        28658,
-        28776
-    );
--- Insertion dans TA_GEO_CORRECT_DOUBLONS des DOS_NUM présents en doublons dans TA_GG_GEO
-MERGE INTO GEO.TEMP_GEO_CORRECT_DOUBLONS a
-    USING(
-        WITH 
-            C_1 AS(
-                SELECT
-                    MIN(a.ID_GEOM) AS ID_GEOM,
-                    a.DOS_NUM
-                FROM
-                    GEO.TA_GG_GEO a
-                WHERE
-                    a.DOS_NUM IN(
-                    101960145,102860526,
-                    103280549,103030163,
-                    203860513,203600428,
-                    163281074,202990438,
-                    103160408,102860525,
-                    202790509,205990488,
-                    103500394,103280411,
-                    161961077,103430570,
-                    205990489,102860124,
-                    102860531,205990493,
-                    203780518,163280679,
-                    205990498,205990487,
-                    205270519, 200900515
-                    )
-                GROUP BY
-                    a.DOS_NUM
-                HAVING
-                    COUNT(a.DOS_NUM) > 1
-            )
-            SELECT
-                a.ID_GEOM,
-                a.DOS_NUM,
-                a.GEOM,
-                'fusionner' AS ACTION
-            FROM
-                GEO.TA_GG_GEO a,
-                C_1 b
-            WHERE
-                a.ID_GEOM = b.ID_GEOM
-        )t
-    ON (a.ID_GEOM = t.ID_GEOM)
-WHEN NOT MATCHED THEN
-    INSERT(a.ID_GEOM, a.DOS_NUM, a.GEOM, a.ACTION)
-    VALUES(t.ID_GEOM, t.DOS_NUM, t.GEOM, t.ACTION);
-
--- Suppression des polygones à supprimer de TEMP_GEO_CORRECTION
+-- Suppression des polygones à supprimer de TA_GG_GEO
 DELETE
 FROM 
-    GEO.TEMP_GEO_CORRECTION a
+    GEO.TA_GG_GEO a
 WHERE
     a.ID_GEOM IN(4547, 83, 85, 2496, 652, 12648);
 
-/* Correction des erreurs de géométrie dans TEMP_GEO_CORRECTION via SDO_UTIL.RECTIFY_GEOMETRY
+/* Correction des erreurs de géométrie dans TA_GG_GEO via SDO_UTIL.RECTIFY_GEOMETRY
 Rappel - les erreurs que cette fonction corrige sont :
 - 13349 : le polygone l'intersecte lui-même ;
 - 13356 : le polygone a des sommets redondants ;
@@ -109,36 +21,36 @@ Rappel - les erreurs que cette fonction corrige sont :
 */
 
 -- Erreur 13343 : suppression des polygones qui sont manifestement des erreurs : 1. ce sont des lignes de type 2003 ; 2.leur suppression n'impacte pas les périmètres des dossiers 
-DELETE FROM GEO.TEMP_GEO_CORRECTION a
+DELETE FROM GEO.TA_GG_GEO a
 WHERE
     SUBSTR(SDO_GEOM.VALIDATE_GEOMETRY_WITH_CONTEXT(a.geom, 0.005), 0, 5) = '13343';
     
 -- Erreur 13028 : correction du SDO_ELEM_INFO_ARRAY de chaque entité
-UPDATE GEO.TEMP_GEO_CORRECTION a
+UPDATE GEO.TA_GG_GEO a
 SET a.geom.SDO_ELEM_INFO = MDSYS.SDO_ELEM_INFO_ARRAY(1,1003,1)
 WHERE
  SDO_GEOM.VALIDATE_GEOMETRY_WITH_CONTEXT(a.GEOM, 0.005) = '13028';
 
 -- Erreur 13349
-UPDATE GEO.TEMP_GEO_CORRECTION a
+UPDATE GEO.TA_GG_GEO a
     SET a.GEOM = SDO_UTIL.RECTIFY_GEOMETRY(a.geom, 0.005)
 WHERE
     SUBSTR(SDO_GEOM.VALIDATE_GEOMETRY_WITH_CONTEXT(a.geom, 0.005), 0, 5) = '13349';
     
 -- Erreur 13356
-UPDATE GEO.TEMP_GEO_CORRECTION a
+UPDATE GEO.TA_GG_GEO a
     SET a.GEOM = SDO_UTIL.RECTIFY_GEOMETRY(a.geom, 0.005)
 WHERE
     SUBSTR(SDO_GEOM.VALIDATE_GEOMETRY_WITH_CONTEXT(a.geom, 0.005), 0, 5) = '13356';
     
 -- Erreur 13367
-UPDATE GEO.TEMP_GEO_CORRECTION a
+UPDATE GEO.TA_GG_GEO a
     SET a.GEOM = SDO_UTIL.RECTIFY_GEOMETRY(a.geom, 0.005)
 WHERE
     SUBSTR(SDO_GEOM.VALIDATE_GEOMETRY_WITH_CONTEXT(a.geom, 0.005), 0, 5) = '13367';
 
 -- Fusion des polygones d'un même dossier
-MERGE INTO GEO.TEMP_GEO_CORRECTION a
+MERGE INTO GEO.TA_GG_GEO a
     USING(
         WITH
             C_1 AS(
@@ -148,8 +60,8 @@ MERGE INTO GEO.TEMP_GEO_CORRECTION a
                     b.CLASSE_DICT,
                     a.DOS_NUM
                 FROM
-                    GEO.TEMP_GEO_CORRECT_DOUBLONS a
-                    INNER JOIN GEO.TEMP_GEO_CORRECTION b ON b.ID_GEOM = a.ID_GEOM
+                    GEO.TA_GEO_CORRECT_DOUBLONS a
+                    INNER JOIN GEO.TA_GG_GEO b ON b.ID_GEOM = a.ID_GEOM
                 WHERE
                     a.ACTION IN('fusionnner',
                                 'fusionnjer',
@@ -198,7 +110,7 @@ MERGE INTO GEO.TEMP_GEO_CORRECTION a
                 ) AS GEOM
             FROM
                 C_1 a,
-                GEO.TEMP_GEO_CORRECTION b
+                GEO.TA_GG_GEO b
             WHERE
                 b.DOS_NUM = a.DOS_NUM
             GROUP BY
@@ -225,13 +137,13 @@ WHEN MATCHED THEN
 -- Suppression des entités ayant servi à la fusion
 DELETE
 FROM 
-    GEO.TEMP_GEO_CORRECTION a
+    GEO.TA_GG_GEO a
 WHERE
     a.DOS_NUM IN (
                     SELECT 
                         DOS_NUM
                     FROM
-                        GEO.TEMP_GEO_CORRECT_DOUBLONS a
+                        GEO.TA_GEO_CORRECT_DOUBLONS a
                     WHERE
                         a.ACTION IN(
                             'fusionnner',
@@ -278,7 +190,7 @@ WHERE
                             SELECT 
                                 ID_GEOM
                             FROM
-                                GEO.TEMP_GEO_CORRECT_DOUBLONS a
+                                GEO.TA_GEO_CORRECT_DOUBLONS a
                             WHERE
                                 a.ACTION IN(
                                     'fusionnner',
@@ -326,7 +238,7 @@ WHERE
 
 -- Passage de certains dossiers/périmètres en clôturés
 -- Résultats attendus : 4 lignes éditées
-UPDATE GEO.TEMP_GEO_CORRECTION a
+UPDATE GEO.TA_GG_GEO a
     SET a.ETAT_ID = 9
 WHERE
     a.DOS_NUM IN(
@@ -344,13 +256,13 @@ WHERE
                 )
 ;
 -- Résultats attendus : 4 lignes éditées
-UPDATE GEO.TEMP_DOSSIER_CORRECTION a
+UPDATE GEO.TA_GG_DOSSIER a
     SET a.ETAT_ID = 9
 WHERE
     a.DOS_NUM IN(
                     SELECT b.DOS_NUM
                     FROM
-                        GEO.TEMP_DOSSIER_CORRECTION b
+                        GEO.TA_GG_DOSSIER b
                     WHERE
                         b.DOS_NUM IN(
                                         202860388,
@@ -363,8 +275,9 @@ WHERE
 ;
 
 -- Création de deux nouveaux dossiers dans TEMP_DOSSIER_CORRECTION. Ces dossiers correspondront aux périmètres présents dans TA_GG_GEO dont le DOS_NUM = 5332, mais ne disposant pas de dossier dans TEMP_DOSSIER_CORRECTION pour le moment.
-INSERT INTO GEO.TEMP_DOSSIER_CORRECTION(SRC_ID,ETAT_ID,USER_ID,FAM_ID,DOS_DC,DOS_PRECISION,DOS_DMAJ,DOS_RQ,DOS_DT_FIN,DOS_PRIORITE,DOS_IDPERE,DOS_DT_DEB_TR,DOS_DT_FIN_TR,DOS_DT_CMD_SAI,DOS_INSEE,DOS_VOIE,DOS_MAO,DOS_ENTR,ORDER_ID,DOS_NUM,DOS_OLD_ID,DOS_DT_DEB_LEVE,DOS_DT_FIN_LEVE,DOS_DT_PREV, DOS_URL_FILE)
+INSERT INTO GEO.TA_GG_DOSSIER(ID_DOS, SRC_ID,ETAT_ID,USER_ID,FAM_ID,DOS_DC,DOS_PRECISION,DOS_DMAJ,DOS_RQ,DOS_DT_FIN,DOS_PRIORITE,DOS_IDPERE,DOS_DT_DEB_TR,DOS_DT_FIN_TR,DOS_DT_CMD_SAI,DOS_INSEE,DOS_VOIE,DOS_MAO,DOS_ENTR,ORDER_ID,DOS_NUM,DOS_OLD_ID,DOS_DT_DEB_LEVE,DOS_DT_FIN_LEVE,DOS_DT_PREV, DOS_URL_FILE)
 SELECT
+    45829,
     a.SRC_ID,
     a.ETAT_ID,
     a.USER_ID,
@@ -384,19 +297,20 @@ SELECT
     a.DOS_MAO,
     a.DOS_ENTR,
     a.ORDER_ID,
-    136500142,
+    163500142,
     a.DOS_OLD_ID,
     a.DOS_DT_DEB_LEVE,
     a.DOS_DT_FIN_LEVE,
     a.DOS_DT_PREV,
     'RECOL/163500142_59350_rue_du_ballon/'
 FROM
-    GEO.TEMP_DOSSIER_CORRECTION a
+    GEO.TA_GG_DOSSIER a
 WHERE
     a.DOS_NUM = 163500137;
 
-INSERT INTO GEO.TEMP_DOSSIER_CORRECTION(SRC_ID,ETAT_ID,USER_ID,FAM_ID,DOS_DC,DOS_PRECISION,DOS_DMAJ,DOS_RQ,DOS_DT_FIN,DOS_PRIORITE,DOS_IDPERE,DOS_DT_DEB_TR,DOS_DT_FIN_TR,DOS_DT_CMD_SAI,DOS_INSEE,DOS_VOIE,DOS_MAO,DOS_ENTR,ORDER_ID,DOS_NUM,DOS_OLD_ID,DOS_DT_DEB_LEVE,DOS_DT_FIN_LEVE,DOS_DT_PREV, DOS_URL_FILE)
+INSERT INTO GEO.TA_GG_DOSSIER(ID_DOS, SRC_ID,ETAT_ID,USER_ID,FAM_ID,DOS_DC,DOS_PRECISION,DOS_DMAJ,DOS_RQ,DOS_DT_FIN,DOS_PRIORITE,DOS_IDPERE,DOS_DT_DEB_TR,DOS_DT_FIN_TR,DOS_DT_CMD_SAI,DOS_INSEE,DOS_VOIE,DOS_MAO,DOS_ENTR,ORDER_ID,DOS_NUM,DOS_OLD_ID,DOS_DT_DEB_LEVE,DOS_DT_FIN_LEVE,DOS_DT_PREV, DOS_URL_FILE)
 SELECT
+    45830,
     a.SRC_ID,
     a.ETAT_ID,
     a.USER_ID,
@@ -416,35 +330,34 @@ SELECT
     a.DOS_MAO,
     a.DOS_ENTR,
     a.ORDER_ID,
-    136500169,  
+    163500169,  
     a.DOS_OLD_ID,
     a.DOS_DT_DEB_LEVE,
     a.DOS_DT_FIN_LEVE,
     a.DOS_DT_PREV,
     'RECOL/163500169_59350_rue_de_la_communaute/'
 FROM
-    GEO.TEMP_DOSSIER_CORRECTION a
+    GEO.TA_GG_DOSSIER a
 WHERE
-    a.DOS_NUM = 136500169;
+    a.DOS_NUM = 163500137;
 
-MERGE INTO GEO.TEMP_GEO_CORRECTION a
+MERGE INTO GEO.TA_GG_GEO a
 	USING(
 			SELECT
 				b.ID_DOS,
 				b.DOS_NUM
 			FROM
-				GEO.TEMP_DOSSIER_CORRECTION b
+				GEO.TA_GG_DOSSIER b
 			WHERE
-				b.DOS_NUM IN(163500137, 136500169)
+				b.DOS_NUM IN(163500142, 163500169)
 		)t
 ON (a.DOS_NUM = t.DOS_NUM)
 WHEN MATCHED THEN
 	UPDATE 
 		SET a.ID_DOS = t.ID_DOS;    
 COMMIT;
-
--- Réactivation de la clé étrangère de TA_GG_GEO qui dispose de l'option ON DELETE CASCADE vers TEMP_DOSSIER_CORRECTION
-EXECUTE IMMEDIATE 'ALTER TABLE GEO.TEMP_GEO_CORRECTION ENABLE CONSTRAINT TMP_GEO_CORRECT_ID_DOS_FK';
+-- Réactivation de la clé étrangère de TA_GG_GEO qui dispose de l'option ON DELETE CASCADE vers TA_GG_DOSSIER
+EXECUTE IMMEDIATE 'ALTER TABLE GEO.TA_GG_GEO ENABLE CONSTRAINT TA_GG_GEO_ID_DOS_FK';
 
 -- En cas d'erreur une exception est levée et un rollback effectué, empêchant ainsi toute insertion de se faire et de retourner à l'état des tables précédent l'insertion.
 EXCEPTION
