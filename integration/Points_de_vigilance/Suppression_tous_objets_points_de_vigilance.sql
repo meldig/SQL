129135258c74f41c8bd2c24222c0952e8c7e038e
BaptisteJacq <50438172+BaptisteJacq@users.noreply.github.com>
2021-02-02 14:46:59 +0100

    coquille

--- integration/Points_de_vigilance/Suppression_tous_objets_points_de_vigilance.sql
+++ integration/Points_de_vigilance/Suppression_tous_objets_points_de_vigilance.sql
@@ -1,22 +1,14 @@
 /*
 Requêtes permettant la suppression de tous les objets, et donc des données, liés aux points de vigilance (tables, contraintes, déclencheurs).
-	1. Suppression de la table pivot permettant d'associer un point de vigilance à un permis de construire ;
-	2. Suppression de la table des permis de construire ;
-	3. Suppression de la table d'audit ;
-	4. Suppression de la table des points de vigilance et de ses métadonnées spatiales ;
+	1. Suppression de la table d'audit ;
+	2. Suppression de la table des points de vigilance et de ses métadonnées spatiales ;
 */
 
--- 1. Suppression de la table pivot permettant d'associer un point de vigilance à un permis de construire ;
-	DROP TABLE G_GEO.TA_GG_POINT_VIGILANCE_PERMIS_CONSTRUIRE CASCADE CONSTRAINTS;
+-- 1. Suppression de la table d'audit ;
+	DROP TABLE G_GESTIONGEO.TA_GG_POINT_VIGILANCE_AUDIT  CASCADE CONSTRAINTS;
 
--- 2. Suppression de la table des permis de construire ;
-	DROP TABLE G_GEO.TA_GG_PERMIS_CONSTRUIRE CASCADE CONSTRAINTS;
-
--- 3. Suppression de la table d'audit ;
-	DROP TABLE G_GEO.TA_GG_POINT_VIGILANCE_AUDIT  CASCADE CONSTRAINTS;
-
--- 4. Suppression de la table des points de vigilance et de ses métadonnées spatiales ;
-	DROP TABLE G_GEO.TA_GG_POINT_VIGILANCE CASCADE CONSTRAINTS;
+-- 2. Suppression de la table des points de vigilance et de ses métadonnées spatiales ;
+	DROP TABLE G_GESTIONGEO.TA_GG_POINT_VIGILANCE CASCADE CONSTRAINTS;
 
 	DELETE 
 	FROM 
