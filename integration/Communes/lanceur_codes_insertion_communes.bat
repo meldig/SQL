c2ba328d4838641b01744ffaac6fa1c63deb58eb
BaptisteJacq <50438172+BaptisteJacq@users.noreply.github.com>
2021-02-09 09:37:43 +0100

    maj du .bat insérant les communes dans une table temporaire

--- integration/Communes/lanceur_codes_insertion_communes.bat
+++ integration/Communes/lanceur_codes_insertion_communes.bat
@@ -1,17 +1,12 @@
 @echo off
-echo Bienvenu dans l insertion des donnees des communes en base !
+echo Bienvenu dans l insertion des donnees des communes simples et associees en base !
 :: Import des communes reçues de l'IGN en base.
 
 :: Déclaration et valorisation des variables
 SET /p chemin_insertion="Veuillez saisir le chemin d'accès du dossier contenant TOUTES les communes de TOUS les départements : "  
-SET /p chemin_normalisation="Veuillez saisir le chemin d'accès du dossier contenant les requêtes sql de normalisation des données en base : "  
 SET /p USER="Veuillez saisir l'utilisateur Oracle : "    
 SET /p MDP="Veuillez saisir le MDP : "    
 SET /p INSTANCE="Veuillez saisir l'instance Oracle :"     
-
-:: 1. Concatenation des fichiers SQL en un seul pour pour permettre l'execution des commandes par Oracle
-type %chemin_normalisation%\traitement_table_import_communes.sql > communes_temp.sql | echo. >> communes_temp.sql ^
-| type %chemin_normalisation%\insertion_des_communes_des_hauts_de_france.sql >> communes_temp.sql | echo. >> communes_temp.sql
 
 :: 2. Se mettre dans l'environnement de QGIS ;
 c:
@@ -37,9 +32,4 @@ ogr2ogr -f OCI -append -sql "SELECT INSEE_COM, NATURE, NOM FROM COMMUNE_ASSOCIEE
 ogr2ogr -f OCI -append -sql "SELECT INSEE_COM, NATURE, NOM FROM COMMUNE_ASSOCIEE_OU_DELEGUEE WHERE SUBSTR(INSEE_COM,0,2) = '62'" OCI:%USER%/%MDP%@%INSTANCE% %chemin_insertion%\62\COMMUNE_ASSOCIEE_OU_DELEGUEE.shp -nln TEMP_COMMUNE_ASSOCIEE_OU_DELEGUEE -lco SRID=2154 -dim 2
 ogr2ogr -f OCI -append -sql "SELECT INSEE_COM, NATURE, NOM FROM COMMUNE_ASSOCIEE_OU_DELEGUEE WHERE SUBSTR(INSEE_COM,0,2) = '80'" OCI:%USER%/%MDP%@%INSTANCE% %chemin_insertion%\80\COMMUNE_ASSOCIEE_OU_DELEGUEE.shp -nln TEMP_COMMUNE_ASSOCIEE_OU_DELEGUEE -lco SRID=2154 -dim 2
 
-:: 7. lancement de SQL plus.
-CD C:/ora12c/R1/BIN
-
-:: 8. Execution de sqlplus. pour lancer les requetes SQL.
-sqlplus.exe %USER%/%MDP%@%INSTANCE% @%chemin_normalisation%\communes_temp.sql
 pause
