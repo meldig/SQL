a94c49a250d5df6b7e6f77b0d1db7bcedb124efb
BaptisteJacq <50438172+BaptisteJacq@users.noreply.github.com>
2021-02-11 14:20:43 +0100

    ajout des codes nécessaires à l'insertion des communes associées
    ajout les fichiers suivants :
    - le lanceur .bat ;
    - le code sql d'intégration des communes associées ;
    
    Mise à jour de la documentation avec les fichiers d'insertion des communes associées.

--- integration/Communes/lanceur_maj_communes_associees_nouvelle_bdtopo.bat
+++ integration/Communes/lanceur_maj_communes_associees_nouvelle_bdtopo.bat
@@ -1,10 +1,10 @@
 @echo off
-echo Bienvenu dans l insertion des communes simples de la nouvelle BdTopo en base !
+echo Bienvenu dans l insertion des communes associees de la nouvelle BdTopo en base !
 echo WARNING : CETTE ETAPE PREND PLACE APRES AVOIR IMPORTE DANS UNE TABLE TEMPORAIRE EN BASE LES COMMUNES DE LA NOUVELLE BDTOPO !
 :: Import des communes reçues de l'IGN en base.
 
 :: Déclaration et valorisation des variables
-SET /p chemin_normalisation="Veuillez saisir le chemin d'accès du dossier contenant la requêtes d insertion des communes simples de la nouvelle BdTopo : "  
+SET /p chemin_normalisation="Veuillez saisir le chemin d'accès du dossier contenant la requêtes d insertion des communes associees de la nouvelle BdTopo : "  
 SET /p USER="Veuillez saisir l'utilisateur Oracle : "    
 SET /p MDP="Veuillez saisir le MDP : "    
 SET /p INSTANCE="Veuillez saisir l'instance Oracle :"     
@@ -16,5 +16,5 @@ SET NLS_LANG=AMERICAN_AMERICA.AL32UTF8
 CD C:/ora12c/R1/BIN
 
 :: 8. Execution de sqlplus. pour lancer les requetes SQL.
-sqlplus.exe %USER%/%MDP%@%INSTANCE% @%chemin_normalisation%\maj_communes_simples_nouvelle_bdtopo.sql
+sqlplus.exe %USER%/%MDP%@%INSTANCE% @%chemin_normalisation%\maj_communes_associees_nouvelle_bdtopo.sql
 pause
