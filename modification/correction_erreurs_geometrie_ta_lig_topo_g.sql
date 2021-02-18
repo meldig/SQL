15f81a3a95540cb8125deb9af6737151e83786dd
BaptisteJacq <50438172+BaptisteJacq@users.noreply.github.com>
2021-02-18 10:43:35 +0100

    création d'un procédure corrigeant les erreurs de géométries de la table TA_LIG_TOPO_G
    les erreurs corrigées dans cette procédure sont celles qui ont été identifiées. Il ne s'agit pas d'une procédure qui corrigerait tous les types d'erreurs.

---
+++ modification/correction_erreurs_geometrie_ta_lig_topo_g.sql
@@ -0,0 +1,44 @@
+SET SERVEROUTPUT ON
+DECLARE
+BEGIN
+SAVEPOINT POINT_SAVE_TA_LIG_TOPO_G;
+
+/*
+Cette procédure a pour objectif de corriger les erreurs de géométrie présentes dans la table TA_LIG_TOPO_G.
+Les erreurs corrigées ci-dessous sont celles identifiées dans la table, ça ne veut pas dire que tous les types d'erreurs sont corrigés. Seuls ceux qui ont été identifiés le sont.
+*/
+
+-- Correction de l'erreur 13342 -> une géométrie de type arc doit obligatoirement disposer d'au moins 3 sommets.
+UPDATE GEO.TEST_GG_LIG_TOPO_G t
+    SET t.geom = SDO_UTIL.SIMPLIFY(t.geom, 0.005, 0.005)
+WHERE
+    SUBSTR(SDO_GEOM.VALIDATE_GEOMETRY_WITH_CONTEXT(t.geom, 0.005), 0, 5) = '13342';
+    
+-- Correction de l'erreur 13356 -> Une géométrie dispose de sommets en doublons
+UPDATE GEO.TEST_GG_LIG_TOPO_G a
+    SET a.geom = SDO_UTIL.RECTIFY_GEOMETRY(a.geom, 0.005)
+WHERE
+    SUBSTR(SDO_GEOM.VALIDATE_GEOMETRY_WITH_CONTEXT(a.geom, 0.005), 0, 5) = '13356';
+
+-- Suppression des géométries NULL
+DELETE 
+FROM 
+	GEO.TEST_GG_LIG_TOPO_G a 
+WHERE 
+	a.geom IS NULL;
+
+-- Suppression des géométries d'un type autre que ligne ou multiligne
+DELETE
+FROM
+    GEO.TEST_GG_LIG_TOPO_G a
+WHERE
+    a.geom.sdo_gtype NOT IN(2002, 2006);
+    
+COMMIT;
+
+-- En cas d'erreur une exception est levée et un rollback effectué, empêchant ainsi toute modification de se faire et de retourner à l'état de la table précédent.
+EXCEPTION
+    WHEN OTHERS THEN
+        DBMS_OUTPUT.PUT_LINE('L''erreur ' || SQLCODE || 'est survenue. Un rollback a été effectué : ' || SQLERRM(SQLCODE));
+        ROLLBACK TO POINT_SAVE_TA_LIG_TOPO_G;
+END;
