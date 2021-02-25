845eabbc577c782df0cf45f1967b003deebb0c7e
BaptisteJacq <50438172+BaptisteJacq@users.noreply.github.com>
2021-02-25 17:59:50 +0100

    maj code après import de la BdTopo2020 en prod

--- integration/Communes/import_communes_en_base.bat
+++ integration/Communes/import_communes_en_base.bat
@@ -10,13 +10,13 @@ SET /p INSTANCE="Veuillez saisir l instance Oracle :"
 
 :: 2. Se mettre dans l'environnement de QGIS ;
 c:
-cd C:\Program Files\QGIS 3.10\bin
+cd C:\Program Files\QGIS 3.16\bin
 
 :: 3. Configurer le système d'encodage des caractères en UTF-8
 SET NLS_LANG=AMERICAN_AMERICA.AL32UTF8
 
 :: 4. Rediriger la variable PROJ_LIB vers le bon fichier proj.db afin qu'ogr2ogr trouve le bon scr
-setx PROJ_LIB "C:\Program Files\QGIS 3.10\share\proj"
+setx PROJ_LIB "C:\Program Files\QGIS 3.16\share\proj"
 
 :: 5. Import des communes simples en base
 ogr2ogr -f OCI -sql "SELECT INSEE_COM, INSEE_DEP, NOM FROM COMMUNE WHERE INSEE_DEP = '02'" OCI:%USER%/%MDP%@%INSTANCE% %chemin_insertion%\02\donnees\ADMINISTRATIF\COMMUNE.shp -nln TEMP_COMMUNES -nlt multipolygon -lco SRID=2154 -dim 2
@@ -27,7 +27,7 @@ ogr2ogr -f OCI -append -sql "SELECT INSEE_COM, INSEE_DEP, NOM FROM COMMUNE WHERE
 
 :: 6. Import des communes associées/déléguées en base
 ogr2ogr -f OCI -sql "SELECT INSEE_COM, NATURE, NOM FROM COMMUNE_ASSOCIEE_OU_DELEGUEE WHERE SUBSTR(INSEE_COM,0,2) = '02'" OCI:%USER%/%MDP%@%INSTANCE% %chemin_insertion%\02\donnees\ADMINISTRATIF\COMMUNE_ASSOCIEE_OU_DELEGUEE.shp -nln TEMP_COMMUNE_ASSOCIEE_OU_DELEGUEE -nlt multipolygon -lco SRID=2154 -dim 2
-ogr2ogr -f OCI -append -sql "SELECT INSEE_COM, NATURE, NOM FROM COMMUNE_ASSOCIEE_OU_DELEGUEE WHERE SUBSTR(INSEE_COM,0,2) = '59'" OCI:%USER%/%MDP%@%INSTANCE% %chemin_insertion%\59\donnees\ADMINISTRATIF\COMMUNE_ASSOCIEE_OU_DELEGUEE.shp -nln TEMP_COMMUNE_ASSOCIEE_OU_DELEGUEE -lco SRID=2154 -dim 2
+ogr2ogr -f OCI -append -sql "SELECT INSEE_COM, NATURE, NOM FROM COMMUNE_ASSOCIEE_OU_DELEGUEE WHERE SUBSTR(INSEE_COM,0,2) = '59'" OCI:%USER%/%MDP%@%INSTANCE% %chemin_insertion%\59\donnees\ADMINISTRATIF\COMUNE_ASSOCIEE_OU_DELEGUEE.shp -nln TEMP_COMMUNE_ASSOCIEE_OU_DELEGUEE -lco SRID=2154 -dim 2
 ogr2ogr -f OCI -append -sql "SELECT INSEE_COM, NATURE, NOM FROM COMMUNE_ASSOCIEE_OU_DELEGUEE WHERE SUBSTR(INSEE_COM,0,2) = '60'" OCI:%USER%/%MDP%@%INSTANCE% %chemin_insertion%\60\donnees\ADMINISTRATIF\COMMUNE_ASSOCIEE_OU_DELEGUEE.shp -nln TEMP_COMMUNE_ASSOCIEE_OU_DELEGUEE -lco SRID=2154 -dim 2
 ogr2ogr -f OCI -append -sql "SELECT INSEE_COM, NATURE, NOM FROM COMMUNE_ASSOCIEE_OU_DELEGUEE WHERE SUBSTR(INSEE_COM,0,2) = '62'" OCI:%USER%/%MDP%@%INSTANCE% %chemin_insertion%\62\donnees\ADMINISTRATIF\COMMUNE_ASSOCIEE_OU_DELEGUEE.shp -nln TEMP_COMMUNE_ASSOCIEE_OU_DELEGUEE -lco SRID=2154 -dim 2
 ogr2ogr -f OCI -append -sql "SELECT INSEE_COM, NATURE, NOM FROM COMMUNE_ASSOCIEE_OU_DELEGUEE WHERE SUBSTR(INSEE_COM,0,2) = '80'" OCI:%USER%/%MDP%@%INSTANCE% %chemin_insertion%\80\donnees\ADMINISTRATIF\COMMUNE_ASSOCIEE_OU_DELEGUEE.shp -nln TEMP_COMMUNE_ASSOCIEE_OU_DELEGUEE -lco SRID=2154 -dim 2
