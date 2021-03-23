b1e326f4d171c7edb72e6fd03eabac70515a1ba2
BaptisteJacq <50438172+BaptisteJacq@users.noreply.github.com>
2021-03-23 08:08:32 +0100

    Correction des géométries de la table TA_SUR_TOPO_G
    * correction des erreurs 13356 et 13349
    * correction des arcs
    * il reste 12 géométries de type collection (2004)

--- modification/correction_erreurs_geometrie_ta_sur_topo_g.sql
+++ modification/correction_erreurs_geometrie_ta_sur_topo_g.sql
@@ -15,7 +15,7 @@ WHERE
     
 -- Erreur 13356 -> géométries disposant de sommets en doublons
 UPDATE GEO.TA_SUR_TOPO_G a
-    SET a.geom = SDO_UTIL.RECTIFY_GEOMETRY(a.geom, 0.005)
+    SET a.geom = SDO_GEOM.SDO_BUFFER(a.geom, 0, 0.001)
 WHERE
     SUBSTR(SDO_GEOM.VALIDATE_GEOMETRY_WITH_CONTEXT(a.geom, 0.005), 0, 5) = '13356';
     
@@ -25,18 +25,19 @@ UPDATE GEO.TA_SUR_TOPO_G a
 WHERE
     SUBSTR(SDO_GEOM.VALIDATE_GEOMETRY_WITH_CONTEXT(a.geom, 0.005), 0, 5) = '13367';
 
--- Erreur 13366 et 13350
+-- Erreur 13366 et 13350 ainsi que les géométries de type 2004
 UPDATE GEO.TA_SUR_TOPO_G a
     SET a.geom = (
                     SELECT
-                        SDO_AGGR_UNION(SDOAGGRTYPE(b.geom, 0.005))
+                        SDO_AGGR_UNION(SDOAGGRTYPE(b.geom, 0.001))
                     FROM
                         GEO.TA_SUR_TOPO_G b
                     WHERE
                         a.objectid = b.objectid
                 )
     WHERE
-        SUBSTR(SDO_GEOM.VALIDATE_GEOMETRY_WITH_CONTEXT(a.geom, 0.005), 0, 5) IN ('13366', '13350');
+        SUBSTR(SDO_GEOM.VALIDATE_GEOMETRY_WITH_CONTEXT(a.geom, 0.005), 0, 5) IN ('13366', '13350')
+        OR a.geom.sdo_gtype = 2004;
 
 -- Suppression des entités ne disposant pas de géométrie
 DELETE FROM GEO.TA_SUR_TOPO_G a
@@ -56,6 +57,42 @@ WHERE a.geom.sdo_gtype = 2001;
 -- Suppression des objets de type ligne
 DELETE FROM GEO.TA_SUR_TOPO_G a
 WHERE a.geom.sdo_gtype = 2002;
+
+-------------------------------------------------------------------------------------------------------------------------------
+-------------------------------------------------------------------------------------------------------------------------------
+
+-- Seconde partie des corrections
+-- Erreur 13349 -> auto intersection - Correction des objets dont les corrections précédentes auraient pu provoquer cette erreur
+UPDATE GEO.TA_SUR_TOPO_G a
+    SET a.geom = SDO_UTIL.RECTIFY_GEOMETRY(a.geom, 0.005)
+WHERE
+    SUBSTR(SDO_GEOM.VALIDATE_GEOMETRY_WITH_CONTEXT(a.geom, 0.005), 0, 5) = '13349';
+    
+-- Erreur 13356 -> géométries disposant de sommets en doublons - correction de cette erreur que les corrections ci-dessus n'ont pu corriger
+UPDATE GEO.TA_SUR_TOPO_G a
+    SET a.geom = SDO_UTIL.RECTIFY_GEOMETRY(a.geom, 0.005)
+WHERE
+    SUBSTR(SDO_GEOM.VALIDATE_GEOMETRY_WITH_CONTEXT(a.geom, 0.005), 0, 5) = '13356';
+
+-- Transformation du type collection en type polygone. Cette requête résulte des transformations dues aux requêtes précédentes
+UPDATE GEO.TA_SUR_TOPO_G a
+    SET a.geom = (
+                    SELECT
+                        SDO_AGGR_UNION(SDOAGGRTYPE(b.geom, 0.001))
+                    FROM
+                        GEO.TA_SUR_TOPO_G b
+                    WHERE
+                        a.objectid = b.objectid
+                )
+    WHERE
+        a.geom.sdo_gtype = 2004;
+
+-- Correction des arcs : tout arc est transformé en lignes simples
+UPDATE GEO.TA_SUR_TOPO_G a
+SET a.geom = sdo_geom.sdo_arc_densify(a.GEOM,0.005,'arc_tolerance=0.05 unit=m')
+WHERE
+    geo.get_info(a.geom,2)=1005
+    OR geo.get_info(a.geom,2)=1003 and geo.get_info(a.geom,3) in (4,2);
 
 COMMIT;
 
