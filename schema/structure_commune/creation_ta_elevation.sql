a9b3c7f17390981c9fea602d36ba0b07da5d1c3c
BaptisteJacq <50438172+BaptisteJacq@users.noreply.github.com>
2021-04-23 13:51:59 +0200

    création du lanceur permettant de créer la table TA_ELEVATION
    - Mise à jour du nom du dossier structure_commune en supprimant l'espace afin de pouvoir appeler les scripts DDL des objets oracle directement depuis ce dossier.
    
    - Le .bat et le .sql sont fonctionnels et ont été testé sur G_GEO DEV ;
    
    - Teste à faire : importer TA_SUR_TOPO_G et les valeurs d'élévation.

--- schema/structure_commune/creation_ta_elevation.sql
+++ schema/structure_commune/creation_ta_elevation.sql
@@ -13,7 +13,7 @@ CREATE TABLE G_GEO.TA_ELEVATION(
 COMMENT ON TABLE G_GEO.TA_ELEVATION IS 'Table permettant d''associer les valeurs d''élévation issues du LIDAR aux objets surfaciques de la table TA_SUR_TOPO_G, permettant d''extruder les bâtiments et d''offrir une meilleure visualisation de la morphologie du territoire';
 COMMENT ON COLUMN G_GEO.TA_ELEVATION.objectid IS 'Clé primaire de la table auto-incrémentée.';
 COMMENT ON COLUMN G_GEO.TA_ELEVATION.elevation IS 'Valeur d''élévation permettant d''extruder les polygones.';
-COMMENT ON COLUMN G_GEO.TA_ELEVATION.fid_libelle IS 'Clé étrangère vers la table TA_LIBELLE permettant de distinguer les valeurs d''élévation entre valeur minimum, valeur moyenne, valeur médiane et valeur maximale.';
+COMMENT ON COLUMN G_GEO.TA_ELEVATION.fid_libelle IS 'Clé étrangère vers la table TA_LIBELLE permettant de distinguer les valeurs d''élévation entre valeur minimale, valeur moyenne, valeur médiane et valeur maximale.';
 COMMENT ON COLUMN G_GEO.TA_ELEVATION.fid_type_elevation IS 'Clé étrangère vers la table TA_LIBELLE permettant de distinguer l''élévation absolue (mesurée au-dessus du niveau de la mer, donc du niveau 0) de l''élévation relative (mesurée au-dessus du sol et donc pouvant différer du niveau 0).';
 COMMENT ON COLUMN G_GEO.TA_ELEVATION.fid_sur_topo_g IS 'Clé étrangère vers la table TA_SUR_TOPO_G permettant d''associer une surface de cette table avec une élévation.';
 COMMENT ON COLUMN G_GEO.TA_ELEVATION.fid_metadonnee IS 'Clé étrangère vers la table TA_METADONNEE permettant de connaître la source, la date d''acquisition et l''organisme créateur de la donnée.';
@@ -49,15 +49,19 @@ REFERENCES G_GEO.TA_METADONNEE(objectid);
 -- 5. Création des index sur les clés étrangères
 CREATE INDEX TA_ELEVATION_FID_LIBELLE_IDX ON G_GEO.TA_ELEVATION(fid_libelle)
     TABLESPACE G_ADT_INDX;
+/
 
 CREATE INDEX TA_ELEVATION_FID_TYPE_ELEVATION_IDX ON G_GEO.TA_ELEVATION(fid_type_elevation)
     TABLESPACE G_ADT_INDX;
+/
 
 CREATE INDEX TA_ELEVATION_FID_SUR_TOPO_G_IDX ON G_GEO.TA_ELEVATION(fid_sur_topo_g)
     TABLESPACE G_ADT_INDX;
+/
 
 CREATE INDEX TA_ELEVATION_FID_METADONNEE_IDX ON G_GEO.TA_ELEVATION(fid_metadonnee)
     TABLESPACE G_ADT_INDX;
+/
 
 -- 6. Affectation du droit de sélection sur les objets de la table aux administrateurs
 GRANT SELECT ON G_GEO.TA_ELEVATION TO G_ADMIN_SIG;
