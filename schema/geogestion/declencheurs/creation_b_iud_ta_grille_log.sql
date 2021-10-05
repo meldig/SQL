8ccfa7d6a23203419425890a604eb315b4114fd2
BaptisteJacq <50438172+BaptisteJacq@users.noreply.github.com>
2021-10-05 17:22:25 +0200

    réorganisation du dossier geogestion + ajouts des objets des grilles

---
+++ schema/geogestion/declencheurs/creation_b_iud_ta_grille_log.sql
@@ -0,0 +1,82 @@
+create or replace TRIGGER G_GESTIONGEO.B_IUD_TA_GRILLE_LOG
+BEFORE INSERT OR UPDATE OR DELETE ON G_GESTIONGEO.TA_GRILLE
+FOR EACH ROW
+DECLARE
+    username VARCHAR2(100);
+    v_id_agent NUMBER(38,0);
+    v_id_insertion NUMBER(38,0);
+    v_id_modification NUMBER(38,0);
+    v_id_suppression NUMBER(38,0);
+BEGIN
+    -- Sélection du pnom
+    SELECT sys_context('USERENV','OS_USER') into username from dual;
+
+    -- Sélection de l'id du pnom correspondant dans la table TA_GG_SOURCE
+    SELECT src_id INTO v_id_agent FROM G_GESTIONGEO.TA_GG_SOURCE WHERE src_libel = username;
+
+    -- Sélection des id des actions présentes dans la table TA_LIBELLE
+    SELECT
+        a.objectid INTO v_id_insertion
+    FROM
+        G_GEO.TA_LIBELLE a
+        INNER JOIN G_GEO.TA_LIBELLE_LONG b ON b.objectid = a.fid_libelle_long
+    WHERE
+        UPPER(b.valeur) = UPPER('insertion');
+
+    SELECT
+        a.objectid INTO v_id_modification
+    FROM
+        G_GEO.TA_LIBELLE a
+        INNER JOIN G_GEO.TA_LIBELLE_LONG b ON b.objectid = a.fid_libelle_long
+    WHERE
+        UPPER(b.valeur) = UPPER('édition');
+
+    SELECT
+        a.objectid INTO v_id_suppression
+    FROM
+        G_GEO.TA_LIBELLE a
+        INNER JOIN G_GEO.TA_LIBELLE_LONG b ON b.objectid = a.fid_libelle_long
+    WHERE
+        UPPER(b.valeur) = UPPER('suppression');
+
+    IF INSERTING THEN -- En cas d'insertion on insère les valeurs de la table TA_GRILLE_LOG, le numéro d'agent correspondant à l'utilisateur, la date de insertion et le type de modification.
+        INSERT INTO G_GESTIONGEO.TA_GRILLE_LOG(id_grille, id_usage, id_etat, id_gestionnaire, geom, date_action, fid_type_action, fid_pnom)
+            VALUES(
+                    :new.objectid,
+                    :new.fid_usage,
+                    :new.fid_etat,
+                    :new.fid_gestionnaire,
+                    :new.geom,
+                    sysdate,
+                    v_id_insertion,
+                    v_id_agent);
+    ELSE
+        IF UPDATING THEN -- En cas de modification on insère les valeurs de la table TA_GRILLE_LOG, le numéro d'agent correspondant à l'utilisateur, la date de modification et le type de modification.
+            INSERT INTO G_GESTIONGEO.TA_GRILLE_LOG(id_grille, id_usage, id_etat, id_gestionnaire, geom, date_action, fid_type_action, fid_pnom)
+            VALUES(
+                    :old.objectid,
+                    :old.fid_usage,
+                    :old.fid_etat,
+                    :old.fid_gestionnaire,
+                    :old.geom,
+                    sysdate,
+                    v_id_modification,
+                    v_id_agent);
+        END IF;
+    END IF;
+    IF DELETING THEN -- En cas de suppression on insère les valeurs de la table TA_GRILLE_LOG, le numéro d'agent correspondant à l'utilisateur, la date de suppression et le type de modification.
+        INSERT INTO G_GESTIONGEO.TA_GRILLE_LOG(id_grille, id_usage, id_etat, id_gestionnaire, geom, date_action, fid_type_action, fid_pnom)
+        VALUES(
+                :old.objectid,
+                :old.fid_usage,
+                :old.fid_etat,
+                :old.fid_gestionnaire,
+                :old.geom,
+                sysdate,
+                v_id_suppression,
+                v_id_agent);
+    END IF;
+    EXCEPTION
+        WHEN OTHERS THEN
+            mail.sendmail('geotrigger@lillemetropole.fr',SQLERRM,'ERREUR TRIGGER - G_GESTIONGEO.B_IUD_TA_GRILLE_LOG','bjacq@lillemetropole.fr','sysdig@lillemetropole.fr');
+END;
---
+++ schema/geogestion/vues/creation_v_grille_creation_last_modification.sql
@@ -0,0 +1,68 @@
+
+  CREATE OR REPLACE FORCE VIEW "G_GESTIONGEO"."V_GRILLE_CREATION_LAST_MODIFICATION" ("ID_GRILLE", "ACTION", "ACTION_PNOM", "DATE_ACTION", "GESTIONNAIRE", "THEME_GRILLE", "ETAT_AVANCEMENT", 
+	 CONSTRAINT "V_GRILLE_CREATION_LAST_MODIFICATION_PK" PRIMARY KEY ("ID_GRILLE") DISABLE) AS 
+  WITH
+        C_1 AS(
+            SELECT
+                a.id_grille,
+                c.valeur AS action,
+                MAX(a.date_action) AS date_action,
+                e.valeur AS theme_grille
+            FROM
+                G_GESTIONGEO.TA_GRILLE_LOG a
+                INNER JOIN G_GEO.TA_LIBELLE b ON b.objectid = a.fid_type_action
+                INNER JOIN G_GEO.TA_LIBELLE_LONG c ON c.objectid = b.fid_libelle_long
+                INNER JOIN G_GEO.TA_LIBELLE d ON d.objectid = a.id_usage
+                INNER JOIN G_GEO.TA_LIBELLE_LONG e ON e.objectid = d.fid_libelle_long
+            WHERE
+                c.valeur = 'édition'
+            GROUP BY
+                a.id_grille,
+                c.valeur,
+                e.valeur
+        )
+        SELECT
+            a.id_grille,
+            a.action,
+            c.src_libel AS action_pnom,
+            a.date_action,
+            f.src_libel AS gestionnaire,
+            a.theme_grille,
+            e.valeur AS etat_avancement
+        FROM
+            C_1 a
+            INNER JOIN G_GESTIONGEO.TA_GRILLE_LOG b ON b.id_grille = a.id_grille AND b.date_action = a.date_action
+            INNER JOIN G_GESTIONGEO.TA_GG_SOURCE c ON c.src_id = b.fid_pnom
+            INNER JOIN G_GEO.TA_LIBELLE d ON d.objectid = b.id_etat
+            INNER JOIN G_GEO.TA_LIBELLE_LONG e ON e.objectid = d.fid_libelle_long
+            INNER JOIN G_GESTIONGEO.TA_GG_SOURCE f ON f.src_id = b.id_gestionnaire
+    UNION ALL
+    SELECT
+        a.id_grille,
+        d.valeur AS action,
+        b.src_libel AS action_pnom,
+        a.date_action,
+        e.src_libel AS gestionnaire,
+        g.valeur AS theme_grille,
+        i.valeur AS etat_avancement
+    FROM
+        G_GESTIONGEO.TA_GRILLE_LOG a
+        INNER JOIN G_GESTIONGEO.TA_GG_SOURCE b ON b.src_id = a.fid_pnom
+        INNER JOIN G_GEO.TA_LIBELLE c ON c.objectid = a.fid_type_action
+        INNER JOIN G_GEO.TA_LIBELLE_LONG d ON d.objectid = c.fid_libelle_long
+        INNER JOIN G_GESTIONGEO.TA_GG_SOURCE e ON e.src_id = a.id_gestionnaire
+        INNER JOIN G_GEO.TA_LIBELLE f ON f.objectid = a.id_usage
+        INNER JOIN G_GEO.TA_LIBELLE_LONG g ON g.objectid = f.fid_libelle_long
+        INNER JOIN G_GEO.TA_LIBELLE h ON h.objectid = a.id_etat
+        INNER JOIN G_GEO.TA_LIBELLE_LONG i ON i.objectid = h.fid_libelle_long
+    WHERE
+        d.valeur = 'insertion';
+
+   COMMENT ON COLUMN "G_GESTIONGEO"."V_GRILLE_CREATION_LAST_MODIFICATION"."ID_GRILLE" IS 'Identifiant d''un élément d''une grille présent dans la table TA_GRILLE.';
+   COMMENT ON COLUMN "G_GESTIONGEO"."V_GRILLE_CREATION_LAST_MODIFICATION"."ACTION" IS 'Type de l''action effectué sur un élément d''une grille.';
+   COMMENT ON COLUMN "G_GESTIONGEO"."V_GRILLE_CREATION_LAST_MODIFICATION"."ACTION_PNOM" IS 'Pnom de l''agent ayant effectué une action sur un élément d''une grille.';
+   COMMENT ON COLUMN "G_GESTIONGEO"."V_GRILLE_CREATION_LAST_MODIFICATION"."DATE_ACTION" IS 'Date de création et de la dernière modification d''un élément d''une grille.';
+   COMMENT ON COLUMN "G_GESTIONGEO"."V_GRILLE_CREATION_LAST_MODIFICATION"."GESTIONNAIRE" IS 'Pnom du gestionnaire de l''élément de la grille.';
+   COMMENT ON COLUMN "G_GESTIONGEO"."V_GRILLE_CREATION_LAST_MODIFICATION"."THEME_GRILLE" IS 'Thème de la grille à laquelle appartient l''élément.';
+   COMMENT ON COLUMN "G_GESTIONGEO"."V_GRILLE_CREATION_LAST_MODIFICATION"."ETAT_AVANCEMENT" IS 'Etat d''avancement de l''élément de la grille (permet à son gestionnaire de savoir où il en est).';
+   COMMENT ON TABLE "G_GESTIONGEO"."V_GRILLE_CREATION_LAST_MODIFICATION"  IS 'Vue proposant la date de création et la date de la dernière modification d''un élément de la grille de TA_GRILLE. Cette vue est utilisée pour visualiser l''évolution des grilles dans les projets qgis.';
