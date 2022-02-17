0d57e5a73fcf5d1b260f3d3fa360b781b8c1c2f6
romain-jault <57357284+romain-jault@users.noreply.github.com>
2022-02-17 10:52:06 +0100

    Create ocs2d_sequence.sql
    Requête SQL pour créer les séquences utilisées par les tables du schéma OCS2D.

---
+++ integration/OCS2D/ocs2d_sequence.sql
@@ -0,0 +1,27 @@
+-- OCS2d_sequence:
+-- Ce fichier contient les sequences 
+
+
+/*
+SEQ_TA_OCS2D_OBJECTID : création de la séquence d'auto-incrémentation de la clé primaire de la table TA_OCS2D
+*/
+
+CREATE SEQUENCE SEQ_TA_OCS2D_OBJECTID START WITH 1 INCREMENT BY 1;
+
+/
+
+/*
+SEQ_TA_OCS2D_MULTIDATE_RELATION_LIBELLE_OBJECTID : création de la séquence d'auto-incrémentation de la clé primaire de la table TA_OCS2D
+*/
+
+CREATE SEQUENCE SEQ_TA_OCS2D_RELATION_LIBELLE_OBJECTID START WITH 1 INCREMENT BY 1;
+
+/
+
+/*
+SEQ_TA_OCS2D_MULTIDATE_RELATION_LIBELLE_OBJECTID : création de la séquence d'auto-incrémentation de la clé primaire de la table TA_OCS2D
+*/
+
+CREATE SEQUENCE SEQ_TA_OCS2D_MILLESIME_OBJECTID START WITH 1 INCREMENT BY 1;
+
+/
