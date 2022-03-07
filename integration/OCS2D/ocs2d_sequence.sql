5d09ad58ed645f92036bc89258f121561fcf3142
romain-jault <57357284+romain-jault@users.noreply.github.com>
2022-03-07 15:36:21 +0100

    Update ocs2d_sequence.sql
    Mise à jour du fichiers contenant les séquences d'incrémentation des clés primaires des tables du schéma G_OCS2D.

--- integration/OCS2D/ocs2d_sequence.sql
+++ integration/OCS2D/ocs2d_sequence.sql
@@ -1,17 +1,32 @@
 -- OCS2d_sequence:
 -- Ce fichier contient les sequences 
+/*
+1. SEQ_TA_OCS2D_GEOM_OBJECTID : création de la séquence d'auto-incrémentation de la clé primaire de la table TA_OCS2D_GEOM
+*/
 
+CREATE SEQUENCE SEQ_TA_OCS2D_GEOM_OBJECTID START WITH 1 INCREMENT BY 1;
+
+/
 
 /*
-SEQ_TA_OCS2D_OBJECTID : création de la séquence d'auto-incrémentation de la clé primaire de la table TA_OCS2D
+2. SEQ_TA_OCS2D_OBJECTID : création de la séquence d'auto-incrémentation de la clé primaire de la table TA_OCS2D_OBJECTID
 */
 
 CREATE SEQUENCE SEQ_TA_OCS2D_OBJECTID START WITH 1 INCREMENT BY 1;
 
 /
 
+
 /*
-SEQ_TA_OCS2D_MULTIDATE_RELATION_LIBELLE_OBJECTID : création de la séquence d'auto-incrémentation de la clé primaire de la table TA_OCS2D
+3. SEQ_TA_OCS2D_MILLESIME_OBJECTID : création de la séquence d'auto-incrémentation de la clé primaire de la table TA_OCS2D_MILLESIME
+*/
+
+CREATE SEQUENCE SEQ_TA_OCS2D_MILLESIME_OBJECTID START WITH 1 INCREMENT BY 1;
+
+/
+
+/*
+4. SEQ_TA_OCS2D_RELATION_LIBELLE_OBJECTID : création de la séquence d'auto-incrémentation de la clé primaire de la table TA_OCS2D_RELATION_LIBELLE
 */
 
 CREATE SEQUENCE SEQ_TA_OCS2D_RELATION_LIBELLE_OBJECTID START WITH 1 INCREMENT BY 1;
@@ -19,9 +34,98 @@ CREATE SEQUENCE SEQ_TA_OCS2D_RELATION_LIBELLE_OBJECTID START WITH 1 INCREMENT BY
 /
 
 /*
-SEQ_TA_OCS2D_MULTIDATE_RELATION_LIBELLE_OBJECTID : création de la séquence d'auto-incrémentation de la clé primaire de la table TA_OCS2D
+5. SEQ_TA_OCS2D_COMMENTAIRE_OBJECTID : création de la séquence d'auto-incrémentation de la clé primaire de la table TA_OCS2D_COMMENTAIRE
 */
 
-CREATE SEQUENCE SEQ_TA_OCS2D_MILLESIME_OBJECTID START WITH 1 INCREMENT BY 1;
+CREATE SEQUENCE SEQ_TA_OCS2D_COMMENTAIRE_OBJECTID START WITH 1 INCREMENT BY 1;
+
+/
+
+/*
+6. SEQ_TA_OCS2D_SOURCE_OBJECTID : création de la séquence d'auto-incrémentation de la clé primaire de la table TA_OCS2D_SOURCE
+*/
+
+CREATE SEQUENCE SEQ_TA_OCS2D_SOURCE_OBJECTID START WITH 1 INCREMENT BY 1;
+
+/
+
+
+/*
+7. SEQ_TA_OCS2D_FAMILLE_OBJECTID : création de la séquence d'auto-incrémentation de la clé primaire de la table TA_OCS2D_FAMILLE
+*/
+
+CREATE SEQUENCE SEQ_TA_OCS2D_FAMILLE_OBJECTID START WITH 1 INCREMENT BY 1;
+
+/
+
+/*
+8. SEQ_TA_OCS2D_LIBELLE_LONG_OBJECTID : création de la séquence d'auto-incrémentation de la clé primaire de la table TA_OCS2D_LIBELLE_LONG
+*/
+
+CREATE SEQUENCE SEQ_TA_OCS2D_LIBELLE_LONG_OBJECTID START WITH 1 INCREMENT BY 1;
+
+/
+
+/*
+9. SEQ_TA_OCS2D_LIBELLE_OBJECTID : création de la séquence d'auto-incrémentation de la clé primaire de la table TA_OCS2D_LIBELLE
+*/
+
+CREATE SEQUENCE SEQ_TA_OCS2D_LIBELLE_OBJECTID START WITH 1 INCREMENT BY 1;
+
+/
+
+/*
+10. SEQ_TA_OCS2D_FAMILLE_LIBELLE_OBJECTID : création de la séquence d'auto-incrémentation de la clé primaire de la table TA_OCS2D_FAMILLE
+*/
+
+CREATE SEQUENCE SEQ_TA_OCS2D_FAMILLE_LIBELLE_OBJECTID START WITH 1 INCREMENT BY 1;
+
+/
+
+/*
+11. SEQ_TA_OCS2D_LIBELLE_COURT_OBJECTID : création de la séquence d'auto-incrémentation de la clé primaire de la table TA_OCS2D_LIBELLE_COURT
+*/
+
+CREATE SEQUENCE SEQ_TA_OCS2D_LIBELLE_COURT_OBJECTID START WITH 1 INCREMENT BY 1;
+
+/
+
+/*
+11. SEQ_TA_OCS2D_RELATION_COMMENTAIRE_OBJECTID : création de la séquence d'auto-incrémentation de la clé primaire de la table TA_OCS2D_LIBELLE_COURT
+*/
+
+CREATE SEQUENCE SEQ_TA_OCS2D_RELATION_COMMENTAIRE_OBJECTID START WITH 1 INCREMENT BY 1;
+
+/
+
+/*
+11. SEQ_TA_OCS2D_RELATION_SOURCE_OBJECTID : création de la séquence d'auto-incrémentation de la clé primaire de la table TA_OCS2D_RELATION_SOURCE
+*/
+
+CREATE SEQUENCE SEQ_TA_OCS2D_RELATION_SOURCE_OBJECTID START WITH 1 INCREMENT BY 1;
+
+/
+
+/*
+11. SEQ_TA_OCS2D_LIBELLE_CORRESPONDANCE_OBJECTID : création de la séquence d'auto-incrémentation de la clé primaire de la table TA_OCS2D_LIBELLE_CORRESPONDANCE
+*/
+
+CREATE SEQUENCE SEQ_TA_OCS2D_LIBELLE_CORRESPONDANCE_OBJECTID START WITH 1 INCREMENT BY 1;
+
+/
+
+/*
+11. SEQ_TA_OCS2D_LIBELLE_RELATION_OBJECTID : création de la séquence d'auto-incrémentation de la clé primaire de la table TA_OCS2D_LIBELLE_RELATION
+*/
+
+CREATE SEQUENCE SEQ_TA_OCS2D_LIBELLE_RELATION_OBJECTID START WITH 1 INCREMENT BY 1;
+
+/
+
+/*
+11. SEQ_TA_OCS2D_LIBELLE_RELATION_FAMILLE_OBJECTID : création de la séquence d'auto-incrémentation de la clé primaire de la table TA_OCS2D_LIBELLE_RELATION_FAMILLE
+*/
+
+CREATE SEQUENCE SEQ_TA_OCS2D_LIBELLE_RELATION_NOMENCLATURE_OBJECTID START WITH 1 INCREMENT BY 1;
 
 /
