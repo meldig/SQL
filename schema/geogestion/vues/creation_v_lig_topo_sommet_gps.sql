4b2d3efc3a502d0bd8586e91c0e28aa3294f1c96
romain-jault <57357284+romain-jault@users.noreply.github.com>
2022-03-22 14:12:11 +0100

    Create creation_v_lig_topo_sommet_gps.sql
    Creation de la vue V_LIG_TOPO_SOMMET_GPS, vue presentant les coordonnées X,Y et Z des sommets des lignes de la table TEM_TA_LIG_TOPO_GPS.

---
+++ schema/geogestion/vues/creation_v_lig_topo_sommet_gps.sql
@@ -0,0 +1,63 @@
+-- 1. creation de la vue V_LIG_TOPO_SOMMET_GPS: Vue presentant les coordonnées X,Y et Z des sommets des lignes de la table TEM_TA_LIG_TOPO_GPS.
+
+CREATE OR REPLACE FORCE VIEW GEO.V_LIG_TOPO_SOMMET_GPS (identifiant,objectid_ligne,cla_inu,cla_li,x,y,z,GEOM,
+CONSTRAINT "V_LIG_TOPO_SOMMET_GPS_PK" PRIMARY KEY ("IDENTIFIANT") DISABLE) AS
+WITH CTE_1 AS
+    (
+    SELECT 
+        a.objectid, 
+        a.cla_inu,
+        b.cla_li,
+        ROUND(t.X,2) AS x, 
+        ROUND(t.Y,2) AS y, 
+        ROUND(t.Z,2) AS z,
+        SDO_GEOMETRY(2001, 2154,
+        SDO_POINT_TYPE(t.X,t.Y,NULL),
+        NULL, NULL) AS GEOM
+    FROM
+        temp_ta_lig_topo_gps a
+    INNER JOIN GEO.TA_CLASSE b ON a.cla_inu = b.cla_inu,
+    TABLE
+        (SDO_UTIL.GETVERTICES(a.geom)) t
+    WHERE
+        a.geo_on_valide = 0
+    )
+    SELECT 
+        rownum AS identifiant,
+        CTE_1.objectid AS objectid_ligne,
+        CTE_1.cla_inu,
+        CTE_1.cla_li,
+        CTE_1.x,
+        CTE_1.y,
+        CTE_1.z,
+        CTE_1.GEOM
+    FROM
+        CTE_1
+;
+
+
+-- 2. Commentaire
+COMMENT ON TABLE GEO.V_LIG_TOPO_SOMMET_GPS IS 'Vue presentant les coordonnées X,Y et Z des sommets des lignes de la table TEM_TA_LIG_TOPO_GPS.';
+
+COMMENT ON COLUMN GEO.V_LIG_TOPO_SOMMET_GPS.IDENTIFIANT IS 'Clé primaire de la vue.';
+COMMENT ON COLUMN GEO.V_LIG_TOPO_SOMMET_GPS.OBJECTID_LIGNE IS 'Objectid de la ligne du sommet.';
+COMMENT ON COLUMN GEO.V_LIG_TOPO_SOMMET_GPS.cla_inu IS 'Cla inu de la ligne du sommet.';
+COMMENT ON COLUMN GEO.V_LIG_TOPO_SOMMET_GPS.cla_li IS 'libelle du cla inu de la ligne.';
+COMMENT ON COLUMN GEO.V_LIG_TOPO_SOMMET_GPS.x IS 'Coordonnée X du point.';
+COMMENT ON COLUMN GEO.V_LIG_TOPO_SOMMET_GPS.y IS 'Coordonnée Y du point.';
+COMMENT ON COLUMN GEO.V_LIG_TOPO_SOMMET_GPS.z IS 'Coordonnée Z du point.';
+COMMENT ON COLUMN GEO.V_LIG_TOPO_SOMMET_GPS.geom IS 'Géométrie du point.';
+
+-- 3. Métadonnées spatiales
+INSERT INTO USER_SDO_GEOM_METADATA(
+    TABLE_NAME, 
+    COLUMN_NAME, 
+    DIMINFO, 
+    SRID
+)
+VALUES(
+    'V_LIG_TOPO_SOMMET_GPS',
+    'geom',
+    SDO_DIM_ARRAY(SDO_DIM_ELEMENT('X', 684540, 719822.2, 0.005),SDO_DIM_ELEMENT('Y', 7044212, 7078072, 0.005)), 
+    2154
+);
